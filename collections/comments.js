2000a24ffd3a764933917835ea29b48a54408e6b
u1 <urban@xiala.net>
2013-07-03 15:11:59 +0200

    comment01-collection added

---
+++ collections/comments.js
@@ -0,0 +1,12 @@
+
+
+CourseComments = new Meteor.Collection("CourseComments");
+
+CourseComments.allow({
+   insert: function (parameter1, Parameter2) {
+    return true;    // everybody's allowed to Chat
+    },
+   remove: function (userId, doc) {
+    return userId && true;   // allow only if UserId is present
+    }
+});
