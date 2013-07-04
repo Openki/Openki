3e82789d632398bffd4ca70025d316433b04b07a
benjamais <benjamais@regengespraeche.ch>
2013-07-04 20:10:54 +0200

    discussion fiitschr öpdeit: endlich richtige reihenfolge, biz aufgeräumt, biz kommentiert..

--- client/views/courses/course.discussion.js
+++ client/views/courses/course.discussion.js
@@ -1,41 +1,35 @@
-  Template.discussion.post = function() {
-  var posts = CourseDiscussions.find({parent_ID: null}, {sort: {time_updated: -1, time_created: -1}});
-    var ordered_posts = [];// = posts.fetch();
-  
-    //ordered_posts = ordered_posts.concat(posts.fetch());
-    //ordered_posts.concat(ordered_posts);
-    
-    
+//load template-content
+Template.discussion.post = function() {
+    //get all first-level posts 
+    var posts = CourseDiscussions.find(
+        {parent_ID: null},
+        {sort: {time_updated: -1, time_created: -1}}
+    );
+    var ordered_posts = [];    
+    // loop over first-level post, search each post for comments, order by most recent
     posts.forEach(function (post){
-        //alert(post.toSource());        
         ordered_posts.push(post);
-        var answers = CourseDiscussions.find({parent_ID: post._id}, {sort: {time_created: -1}});
-        //ordered_posts.push(answers);        
-        answers.forEach(function (answer){
-            ordered_posts.push(answer);
+        var comments = CourseDiscussions.find({parent_ID: post._id}, {sort: {time_created: -1}});
+        comments.forEach(function (comment){
+            ordered_posts.push(comment);
         });
-        //ordered_posts.push(answers);
     });
-    //alert(ordered_posts.toSource());
+    //return array with proper order
     return ordered_posts;
-    
-    //return CourseDiscussions.find({course_ID:Session.get("selected_course")}, {sort: {time_updated: -1, time_created: -1}});
- 
 };
 
-Template.write_post.events({
+Template.postDialog.showPostDialog = function () {
+    return Session.get("showPostDialog");
+};  
+
+Template.writePostDialog.events({
     'click input.add': function () {
         var now = new Date();
         var timestamp = now.getTime();
         var user = Meteor.userId();
         var course = Session.get("selected_course");
-        //alert("isanswer: "+message.elements["message_isanswer"].checked);
+        
         if(Session.get("postID")){
-        //CourseDiscussions.update({_id:Session.get("CommentID")},
-        //    {$set:{
-        //        "message_updated": timestamp
-        //    }}
-        //);
             CourseDiscussions.insert({
                 "parent_ID":Session.get("postID"),
                 "course_ID":course,
@@ -44,42 +38,39 @@ Template.write_post.events({
                 "title":$("#post_title").val(),
                 "text":$("#post_text").val()
             });
-            CourseDiscussions.update({_id:Session.get("postID")},
-                {$set:{
-                    "time_updated":timestamp
-                }}
+            CourseDiscussions.update(
+                {_id:Session.get("postID")},
+                {$set:{"time_updated":timestamp}}
             );
         }else{
             CourseDiscussions.insert({
                 "course_ID":course,
                 "time_created":timestamp,
+                "time_updated":timestamp,
                 "user_ID":user,
                 "title":$("#post_title").val(),
                 "text":$("#post_text").val()
             });
         }
-      
+        //reset session variables, hide dialog
         Session.set("showPostDialog", false);
         Session.set("postID", false);
     },
+    //reset session variables, hide dialog
     'click input.cancel': function () {
         Session.set("showPostDialog", false);
         Session.set("postID", false);
     }
-  });
+});
 
-Template.postDialog.showPostDialog = function () {
-    return Session.get("showPostDialog");
-  };  
-  
 Template.discussion.events({
+    //comment existing post
     'click input.answer': function (template) {
-      //alert(this.toSource());
-      Session.set("postID", this._id);      
-      Session.set("showPostDialog", true);
-      //alert("ID: "+document.forms[2].elements["message_id"].value);
+        Session.set("postID", this._id);      
+        Session.set("showPostDialog", true);
     },
-    'click input.showDialog': function () {
+    //write new post
+    'click input.write': function () {
         Session.set("showPostDialog", true);
     }
 });
