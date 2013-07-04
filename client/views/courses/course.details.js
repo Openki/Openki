fbff16aa6ed1ccfcc62bf7f3a6a090bfbae80066
lazymazy <marcel@kubaa.ch>
2013-07-04 05:37:04 +0200

    subscriber-datenbank entfernt, direkt der course-datenbank angehaengt, verschiedene listen-aufrufe

--- client/views/courses/course.details.js
+++ client/views/courses/course.details.js
@@ -14,27 +14,25 @@
     },
     
     'click input.get_subscriber': function () {
-    	// für Kurs anmelden
-     	     CourseSubscriptions.insert({user_id: Meteor.userId(), course_id: Session.get("selected_course")});
+    	// für Kurs anmelden (array: subscribers)
+     	  Courses.update(Session.get("selected_course"), {$addToSet:{subscribers: Meteor.userId()}});
     	 
     },
     
     'click input.remove_subscriber': function () {
     	// von Kurs abmelden
-    	// ist so lustig verschachtelt weil nur über userid gelöscht werden darf
-    	    CourseSubscriptions.remove({_id: CourseSubscriptions.findOne({user_id: Meteor.userId(), course_id: Session.get("selected_course")})._id});
-    },
+    	Courses.update(Session.get("selected_course"), {$pull:{subscribers: Meteor.userId()}});
+       },
     
     'click input.get_organisator': function () {
-    	// Orga werden
+    	// Orga werden 
     	Courses.update(Session.get("selected_course"), {$set:{organisator: Meteor.userId()}});
     },
     
         
     'click input.remove_organisator': function () {
     	// Orga künden
-    	// ist so lustig verschachtelt weil nur über userid gelöscht werden darf
-    	    Courses.update(Session.get("selected_course"), {$set:{organisator: ""}});
+      	Courses.update(Session.get("selected_course"), {$set:{organisator: ""}});
       },
     
     
@@ -62,18 +60,20 @@
   });
   
   Template.coursedetails.subscribers = function() {
-  	
-  	 return CourseSubscriptions.find({course_id: Session.get("selected_course")}, {sort: {score: -1, name: 1}});
-  }
+  	  //Anmeldungen auslesen
+  	 return Courses.findOne(Session.get("selected_course")).subscribers;
+ }
 
      Template.coursedetails.isSubscribed = function () {
- 	 var my_sub = CourseSubscriptions.findOne({user_id: Meteor.userId(), course_id: Session.get("selected_course")});
- 	  if (my_sub){
+     	//ist User im subscribers-Array?
+     	 if(Courses.findOne(Session.get("selected_course")).subscribers){
+     	    if(Courses.findOne(Session.get("selected_course")).subscribers.indexOf(Meteor.userId())!=-1){
  	  	  return  true;
- 	  }else{
+ 	    }else{
  	  	return false; 
+ 	    }
  	  }
-  };
+     };
 
 
    Template.coursedetails.organisator = function() {
@@ -101,7 +101,6 @@
 // var time_created= format_date(course.time_created);
  var time_created= course.time_created;
     return course && {name: course.name, desc: course.description, tags: course.tags, score: course.score,  createdby: createdby, time_created: time_created};
- 
   }};
   
   
--- client/views/courses/course.list.js
+++ client/views/courses/course.list.js
@@ -1,29 +1,40 @@
+get_courselist=function(listparameters){
+
+	if(listparameters.courses_from_userid){
+		var results=Courses.find({ $or : [ { "organisator" : listparameters.courses_from_userid}, {"subscribers":listparameters.courses_from_userid} ]}, {sort: {time_created: -1, name: 1}});
+	}else{
+		var results=Courses.find({}, {sort: {time_created: -1, name: 1}});
+	}
+	
+     for(m = 0; m < results.count(); m++){
+	   //  results.db_objects[m].createdby=display_username(results.db_objects[m].createdby);	 
+	    results.db_objects[m].time_created=format_date(results.db_objects[m].time_created);	    
+	
+	    if(results.db_objects[m].subscribers){
+		    results.db_objects[m].subscriber_count=  results.db_objects[m].subscribers.length;	    
+	    }
+	    if(results.db_objects[m].organisator){
+		    results.db_objects[m].has_organisator=1;	    
+	    }else{
+		    results.db_objects[m].has_organisator=0;	   
+	    }
+	   
+     }
+    return results;
+  
+}
+
 
 /* ------------------------- Course-list ------------------------- */
 
   Template.courselist.courses = function () {
+
     // gib alle kurse zur�ck, zuerst umgekehrt nach score sortiert und
     // dann nach name sortiert.
-    
-    var results=Courses.find({}, {sort: {time_created: -1, name: 1}});
-    for(m = 0; m < results.count(); m++){
-   //  results.db_objects[m].createdby=display_username(results.db_objects[m].createdby);	 
-    results.db_objects[m].time_created=format_date(results.db_objects[m].time_created);	    
-   results.db_objects[m].subscriber_count=CourseSubscriptions.find({course_id: results.db_objects[m]._id}).count();	    
-   //console.error(results.db_objects[m].organisator);
-    if(results.db_objects[m].organisator){
-    results.db_objects[m].has_organisator=1;	    
-    	}else{
-    results.db_objects[m].has_organisator=0;	   
-   }
-   
-    }
-    return results;
- //alert (results.toSource());
-  //  return Courses.find({}, {sort: {score: -1, name: 1}});
- 
+   return get_courselist({});
   };
 
+  
 
 
 /* ------------------------- Course ------------------------- */
@@ -37,6 +48,28 @@
     // bin ich der "hovered_course", dann gib mir im html die class "hovered"
     return Session.equals("hovered_course", this._id) ? "hovered" : '';
   };
+  
+   
+  Template.course.is_subscribed = function () {
+   	   if(Meteor.userId()){
+        	 if(this.subscribers){
+     	    if(this.subscribers.indexOf(Meteor.userId())!=-1){
+ 	  	  return  true;
+ 	    }else{
+ 	  	return false; 
+ 	    }
+ 	  }   
+   }
+   };
+
+   Template.course.is_organisator = function () {
+ 	  if (this.organisator==Meteor.userId()){
+ 	  	  return  true;
+ 	  }else{
+ 	  	return false; 
+ 	  }
+   };
+
 
 
 
