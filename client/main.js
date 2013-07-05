bcb2c16e25c98556d23e4117408f06f7ff571271
benjamais <benjamais@regengespraeche.ch>
2013-07-05 14:05:22 +0200

    merge because of conflicts

--- client/main.js
+++ client/main.js
@@ -15,10 +15,26 @@ Template.maincontent.route_is = function (data,options) {
 		return options.inverse( this );
     };
 
-Template.userspace.events({
+Template.login.events({
     'submit #form_login':function (){
-        Meteor.loginWithPassword($("#email").val(), $("#password").val(), function (error){
-            alert("ups, falsch.. "+error);
+        //alert("mail: "+$("#email").val()+" passwd: "+$("#password").val());
+        Meteor.loginWithPassword($("#email").val(), $("#password").val(), function(error){
+            if(error){
+                alert(error);
+            }
+            
         });
+        return false;
+    }
+});
+Template.register.events({
+    'submit #form_register':function (){
+        //alert("mail: "+$("#register_email").val()+" passwd: "+$("#register_password").val());
+        Accounts.createUser({username:$("#register_username").val(),email:$("#register_email").val(), password:$("#register_password").val()} , function(error){
+            if(error){
+                alert(error);
+            }
+        });
+        return false;
     }
 });
