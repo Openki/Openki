30419f840d83c067b3a5cf99406eda02e576b176
Stephan Balmer <sb@hava>
2013-07-09 18:58:58 +0200

    New test data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 
 
 Meteor.startup(function () {
- createCategoriesIfNone();
  createCoursesIfNone();
 });
 
@@ -34,58 +33,70 @@ function createCoursesIfNone(){
   }
 }
 
-function createCategoriesIfNone(){
-    // das gleiche für Kategorien
- if (Categories.find().count() === 0) {
-        createCategories();
-  }
+
+/* TESTING: Get user object for name and create it if it doesn't exist */
+function ensureUser(name) {
+		var user_prototype = {username: name}
+		var user
+		while (!(user = Meteor.users.findOne(user_prototype))) { // Legit
+			Accounts.createUser({username: name, email: (name+"@schuel.example").toLowerCase(), password: name});
+		}
+        return user;
 }
 
-
-function createCategories() {
-    var names = ["Nerdy Courses",
-        "Tesla Shit",
-        "Walter",
-        "Standard"];
-    
-    for (var i = 0; i < names.length; i++)
-        Categories.insert({name: names[i]});
+/* TESTING: Get category object for name and create it if it doesn't exist */
+function ensureCategory(name) {
+		var category_prototype = {name: name}
+		var category
+		while (!(category = Categories.findOne(category_prototype))) { // Legit
+			Categories.insert(category_prototype)
+		}
+        return category;
 }
 
-function get_category_id(index) {
-        var category_by_name = Categories.find().fetch();
-        return category_by_name[index]._id;
-    }
 
 function createCourses(){
-    // erstelle ein String-Array f�r die Namen der Kurse
-    var names = ["ASZ Geek-Noob-Nerd Treffen",
-                 "Deutschkurs",
-                 "Photoshop für Fortgeschrittene",
-                 "Onanie",
-                 "Nikola Tesla - Ein Leben in Einsamkeit",
-                 "Unbenannter Kurs"];
-
-    //erstelle ein String-Array f�r die Kursbeschreibungen
-    var description = ["ein treffen für alle die sich für technologie und so interessieren",
-                 "German: Ja, Nein, Vielleicht, Gut gut.",
-                 "How to put the Bling-Effect.",
-                 "DIY or DIE!",
-                 "Wir werden einen Text über Nikola Teslas Kindheit lesen, anhand dessen wir sein Werk in in einen Historischen Kontext zu stellen versuchen.",
-                 "Bitte hier Kursbeschreibung eintragen"];
-    
-    
-
-    var categories = [get_category_id(0),
-                get_category_id(2),
-                get_category_id(0),
-                get_category_id(0),
-                get_category_id(1),
-                get_category_id(2)]; 
-
-    // erstelle so viele Kurse, wie der Namens-Array lang ist
-    for (var i = 0; i < names.length; i++)
-      Courses.insert({name: names[i], createdby: "ov8zqBLipEQma5DLy", time_created: 1372810780636, time_changed: 1372810780636, categories: categories[i], description: description[i], score: Math.floor(Random.fraction()*10)*5, subscribers_min: 2, subscribers_max: 8, subscribers:[]});
+	var testcourses = [{
+			'name':        'Meditation & Yoga/Qi-Gong',
+			'categories': ['Sport', 'Gspürschmi'],
+			'createdby':   'FeeLing',
+			'description': 'Es wäre toll, regelmässig an einem Morgen zusammen zu Meditieren und Yoga/Qi-Gong zu machen. Mir selber fehlt die Selbstdisziplin und für Yoga/Qi-Gong auch das Knowhow, darum wäre eine kleine Gruppe toll. Ein extra Raum wäre toll, geht aber eigentlich überall.'
+		},{
+			'name':        'Aikido',
+			'categories': ['Sport', 'Kampfkunst'],
+			'createdby':   'Kampfhippie',
+			'description': 'Würde sehr gerne regelmässig Aikido trainieren. Wenn eine kleine Gruppe zustande käme, könnte ich sogar vielleicht einen Trainer und ein Dojo auftreiben. Finde Aikido eine der schönsten Kampfsportarten, weil sie versucht auf das Gegenüber einzugehen und den Konflikt zu lösen ohne den anderen Menschen zu zerstören. Youtube-Video Wikipedia'
+		},{
+			'name':        'Garten',
+			'categories': ['Garten', 'Biologie'],
+			'createdby':   'Greendampf',
+			'description': 'Regelmässig zusammen gärtnern und miteinander Knowhow, Infrastruktur, Beziehungen, Samen, Planzen austauschen. Verschiedene schon existierende freie Gärten unterstützen, die Nahrungs- und Heilmittel produzieren. Hätte Zugang zu 2-3 Gärten, die noch Kapazität für motivierte Gärtner hätten.'
+		},{
+			'name':        'Game Design mit Unity',
+			'categories': ['Design', 'Computer'],
+			'createdby':   'Seee',
+			'description': 'Könnte gerne eine ganzheitliche Einführung in die Konzeption, die Gestaltung und die technische Realisierung eines Games anbieten. Der Kurs würde einen Bogen spannen von der Ideenentwickling hin zum Spielmechanik-Entwerfen bis zur Realisierung eines spielbaren Games und eine einfache Einführung in folgende Disziplinen beinhalten: * Spieltheorie * Spielidee * Spielmechanik * Storytelling * Concept Art * Charakterentwicklung * Zeichnen und Bildbearbeitung mit Photoshop * Usability * Prototyping und Gamedesign mit Unity Gameengine * Leveldesign * 3D-Modelling und -Animation mit Blender * Programmieren mit JavaScript und C-Sharp * Sound-Design mit Audacity * Polishing '
+		},{
+			'name':        '',
+			'categories': ['', ''],
+			'createdby':   '',
+			'description': ''
+	}]
+	
+	_.each(testcourses, function(course) {
+		if (!course.createdby) return; // Don't create courses that don't have an creator name
+		for (var i=0; i < course.categories.length; i++) {
+			course.categories[i] = ensureCategory(course.categories[i])._id
+		}
+		course.createdby = ensureUser(course.createdby)._id
+		course.score = Math.floor(Random.fraction()*20)
+		course.subscribers_min= Math.floor(Random.fraction()*5)
+		course.subscribers_max= course.subscribers_min + Math.floor(Random.fraction()*20)
+		course.subscribers = []
+		course.time_created = 1372810780636
+		course.time_changed = 1372810780636
+		Courses.insert(course)
+	})
 }
 
 
