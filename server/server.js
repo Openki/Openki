5ab68ddb691a73cff9450d68f3efc27645209fa0
u1 <urban@xiala.net>
2013-07-11 00:34:27 +0200

    minParticipants set in testdata to 75% as undefined

--- server/server.js
+++ server/server.js
@@ -259,11 +259,11 @@ function createCourses(){
 		}
 		course.createdby = ensureUser(course.createdby)._id
 		course.score = Math.floor(Random.fraction()*Random.fraction()*30)
-		course.subscribers_min = humandistrib()
+		course.subscribers_min = Random.fraction() > 0.25 ? undefined : humandistrib()
 		course.subscribers_max = Random.fraction() > 0.5 ? undefined : course.subscribers_min + Math.floor(course.subscribers_min*Random.fraction())
 		course.subscribers = []
 		course.time_created = new Date(new Date().getTime()-Math.floor(Random.fraction()*80000000000))
-		course.region = Random.fraction() > 0.85 ? 'Testistan' : 'Spilistan'
+		course.region = Random.fraction() > 0.85 ? '9JyFCoKWkxnf8LWPh' : 'EZqQLGL4PtFCxCNrp'
 		Courses.insert(course)
 	})
 }
