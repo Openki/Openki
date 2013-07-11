785255a99384490115933e9ab3c49f3d7c753864
Stephan Balmer <sb@hava>
2013-07-12 01:48:36 +0200

    Allow creating new courses in course edit

--- client/views/courses/course.edit.js
+++ client/views/courses/course.edit.js
@@ -8,6 +8,7 @@ Template.course_edit.available_roles = function() {
 
 /* Emit 'checked' string if id shows up as member or property of cats */
 Template.course_edit.checked = function(id, cats) {
+	if (cats === undefined) return;
 	if (cats.length) {
 		return cats.indexOf(id) >= 0 ? 'checked' : ''
 	} else {
@@ -15,10 +16,9 @@ Template.course_edit.checked = function(id, cats) {
 	}
 }
 
-
 Template.course_edit.events({
 	'click input.save': function () {
-		var course = this
+		var course = this._id ? this : { roles: {} }
 		_.each(Roles.find().fetch(), function(roletype) {
 			var type = roletype.type
 			var should_have = roletype.preset || document.getElementById('role_'+type).checked;
@@ -26,18 +26,22 @@ Template.course_edit.events({
 			if (have && !should_have) delete course.roles[type];
 			if (!have && should_have) course.roles[type] = roletype.protorole
 		})
-	
-		Courses.update(
-			this._id, {
-			$set: {
-				description: $('#editform_description').val(),
-				categories: $('#editform_categories input:checked').map(function(){ return this.name}).get(),
-				name: $('#editform_name').val(),
-				subscribers_min: $('#editform_subscr_min').val(),
-				subscribers_max: $('#editform_subscr_max').val(),
-				roles: course.roles
-			}
-		});
+
+		var ofcourse = {
+			description: $('#editform_description').val(),
+			categories: $('#editform_categories input:checked').map(function(){ return this.name}).get(),
+			name: $('#editform_name').val(),
+			subscribers_min: $('#editform_subscr_min').val(),
+			subscribers_max: $('#editform_subscr_max').val(),
+			roles: course.roles
+		}
+
+		if (this._id) {
+			Courses.update(this._id, { $set: ofcourse })
+		} else {
+			Courses.insert(ofcourse)
+		}
+
 		Session.set("isEditing", false);
 	},
 	
