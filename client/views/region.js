3fa8a6ad9c19c4adacd8c8454058c0bef1ed310c
u1 <urban@xiala.net>
2013-07-12 13:45:15 +0200

    regions selector dded all regions and style

--- client/views/region.js
+++ client/views/region.js
@@ -4,6 +4,13 @@ Template.region_sel.helpers({
 	}
 });
 
+Template.region_sel.helpers({
+	region: function(){
+		var region = Regions.findOne(Session.get('region'))
+		return region ? region.name : 'All regions'
+	}
+});
+
 Template.region_sel.events({
 	'click a.regionselect': function(){
 			Session.set('region', this._id)
