a50d2fe373482bd3567018df89cd87700a5bac2d
u1 <urban@xiala.net>
2013-10-21 02:24:00 +0200

    adding some zurich locations to testdata

---
+++ server/locations.rawdata.js
@@ -0,0 +1,230 @@
+locations = [{
+	'name':        		'Kasama',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['infoshop'],
+	'maxPeople': 		'40',
+	'maxWorkplaces':	'15',
+	'adress': 			'Militärstrasse 87a, 8004 Zürich, Switzerland',
+	'route':			'Bus 31 oder 32 bis Militär/Langstrasse, dann zu Fuss Militärstrasse Richtung HB folgen, rechte Strassenseite bis 87a, im Hinterhof',
+	'description': 		'Lokaler Infoladen seit 1990',
+	'contact':			{
+		'meetings':			'Kasama-VV: Jeden 1.Sonntag im Monat, 18:00-21:00',
+		'email': 			'info@kasama.ch',
+		'web': 				'http://kasama.ch',
+		'fon': 				'044 271 90 09'
+	},
+	'picture': 			'zurich/locations/kasama.jpg',
+	'infra': 			{
+		'projectionScreen':	'1',
+		'projector': 		'1',
+		'audio': 			'0',
+		'blackboard': 		'0',
+		'whiteboard': 		'0',
+		'flipchart': 		'0',
+		'internet': 		'1',
+		'drink': 			'1',
+		'kitchen': 			'1',
+		'stage': 			'0',
+		'specials': 		'two computers/workstations, big kitchen, bibliothek'
+	},
+	'createdby': 		'FeeLing',
+	'hosts': 			{
+		'noContact':	['Kampfhippie'],
+		'contact':		['FeeLing', 'greg']  	// how to do it nicer?
+	}
+},
+{
+	'name':        		'SGMK MechArtLab',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['hackerspace'],
+	'maxPeople': 		'20',
+	'maxWorkplaces':	'20',
+	'adress': 			'Hohlstrasse 52, 8004 Zürich',
+	'route':			'3 min von HelvetiaPlatz, (gleiches Gebäude wie Pusterla Elektronik)',
+	'description': 		'Das MechArtLab beinhaltet Werkstatt, Schulungsraum, Techn. Bibliothek und Lager und bietet Platz für selbstorganisierte Gruppentreffen, Workshops, Jugendelektronikwerkstatt, uvm.',
+	'contact':			{
+		'meetings':			'',
+		'email': 			'',
+		'web': 				'http://www.sgmk-ssam.ch/mechartlab/',
+		'fon': 				''
+	},
+	'picture': 			'http://www.sgmk-ssam.ch/files/images/mechartlab/180820101294.jpg',
+	'infra': 			{
+		'projectionScreen':	'1',
+		'projector': 		'0',
+		'audio': 			'0',
+		'blackboard': 		'0',
+		'whiteboard': 		'1',
+		'flipchart': 		'0',
+		'internet': 		'1',
+		'drink': 			'0',
+		'kitchen': 			'0',
+		'stage': 			'0',
+		'specials': 		'Lots of parts and tools, Ätzanlage, Oszis, Drucker'
+	},
+	'createdby': 		'Sandra',
+	'hosts': 			{
+		'noContact':	[''],
+		'contact':		['Sandra']
+	}
+},
+
+{
+	'name':        		'ABS',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['squat', 'venue'],
+},
+{
+	'name':        		'ASZ-Badenerstrasse',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+{
+	'name':        		'ASZ-Rauti',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+{
+	'name':        		'Siebbdrucki1',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['workshop'],
+},
+{
+	'name':        		'Siebdrucki2',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+{
+	'name':        		'Offene Nähstube Pomon',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['workshop'],
+},
+{
+	'name':        		'Gemeinschaftsgarten SeedCity',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['workshop'],
+},
+{
+	'name':        		'ortoloco-Gemüsefeld',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['workshop'],
+},
+{
+	'name':        		'Stadiongarten',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['workshop'],
+},
+{
+	'name':        		'Dynamo SoundLab',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['workshop'],
+},
+{
+	'name':        		'Dynamo Tanzdach',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['sport'],
+},
+{
+	'name':        		'Designwerkstatt',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['workshop'],
+},
+{
+	'name':        		'A4',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['infoshop'],
+},
+{
+	'name':        		'Fermento',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['infoshop'],
+},
+{
+	'name':        		'K-Set',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+{
+	'name':        		'Lugisland CCCZH',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['hackerspace'],
+},
+{
+	'name':        		'Dock18',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['hackerspace', 'venue'],
+},
+{
+	'name':        		'Colab',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		['hackerspace'],
+},
+{
+	'name':        		'Shedhalle',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+{
+	'name':        		'Kunstraum Walcheturm',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+{
+	'name':        		'Corner College',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+{
+	'name':        		'Meine Grosse Stube',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+{
+	'name':        		'Kaffee Zähringer',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+{
+	'name':        		'',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+},
+
+
+
+{
+	'name':        		'',
+	'region': 			'J6GDhEEvdmdSMzPPF',
+	'categories': 		[''],
+	'maxPeople': 		'',
+	'maxWorkplaces':	'',
+	'adress': 			'',
+	'route':			'',
+	'description': 		'',
+	'contact':			{
+		'meetings':			'',
+		'email': 			'',
+		'web': 				'',
+		'fon': 				''
+	},
+	'picture': 			'',
+	'infra': 			{
+		'projectionScreen':	'',
+		'projector': 		'',
+		'audio': 			'',
+		'blackboard': 		'',
+		'whiteboard': 		'',
+		'flipchart': 		'',
+		'internet': 		'',
+		'drink': 			'',
+		'kitchen': 			'',
+		'stage': 			'',
+		'specials': 		''
+	},
+	'createdby': 		'',
+	'description': 		'',
+	'hosts': 			{
+		'noContact':	[''],
+		'contact':		['']
+	}
+}]
