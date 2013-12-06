33ee655cc47ad28885c8ccf7b82f6c71ba3c0329
Stephan Balmer <sb@cis.ch>
2013-12-06 20:52:45 +0100

    remove obsolete functions

--- client/lib/functions.js
+++ client/lib/functions.js
@@ -5,14 +5,6 @@ get_timestamp = function (){
 	return now.getTime();
 }
 
-format_date= function (date){
-	var d= new Date(date);
-	var curr_date = d.getDate();
-	var curr_month = d.getMonth() + 1; //Months are zero based
-	var curr_year = d.getFullYear();
-	var date_string=curr_date + "." + curr_month + "." + curr_year;
-	return date_string;
-}
 
 display_coursename = function (courseid){
   var course = Courses.findOne({_id:courseid});
@@ -50,13 +42,12 @@ Handlebars.registerHelper('username', function (userid){
   }
 })
 
+
 Handlebars.registerHelper('dateformat', function(date) {
 	// We'll need a date formatter at some point
 	//if (date) return date.toDateString();
     if (date) return date.getDate()+"."+(date.getMonth()+1)+"."+date.getFullYear();
 });
-
-
 
 
 Handlebars.registerHelper('timeformat', function(date) {
@@ -65,60 +56,6 @@ Handlebars.registerHelper('timeformat', function(date) {
 
     if (date) return ("0"+date.getHours()).slice(-2)+":"+("0"+date.getMinutes()).slice(-2);
 });
-
-
-var trimInput = function(val) {
-    return val.replace(/^\s*|\s*$/g, "");
-  }
-
-Handlebars.registerHelper('ifCond', function (v1, operator, v2) {
-
-    switch (operator) {
-        case '==':
-            return (v1 == v2) ? true : false;
-            break;
-        case '===':
-            return (v1 === v2) ? true : false;
-            break;
-    }
-    //return options.inverse(this);
-});
-
-Handlebars.registerHelper('unlessCond', function (v1, operator, v2, options) {
-
-    switch (operator) {
-        case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
-            break;
-        case '===':
-            return (v1 === v2) ? options.fn(this) : options.inverse(this);
-            break;
-        case '<':
-            return (v1 < v2) ? options.fn(this) : options.inverse(this);
-            break;
-        case '<=':
-            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-            break;
-        case '>':
-            return (v1 > v2) ? options.fn(this) : options.inverse(this);
-            break;
-        case '>=':
-            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-            break;
-        case '||':
-            return (v1 || v2) ? options.fn(this) : options.inverse(this);
-            break;
-        case '&&':
-            return (v1 && v2) ? options.fn(this) : options.inverse(this);
-            break;
-        default:
-            return options.inverse(this)
-            break;
-    }
-    //return options.inverse(this);
-});
-
-
 
 
 Handlebars.registerHelper('isNull', function(val) {
