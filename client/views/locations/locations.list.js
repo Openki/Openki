86cce1c2c913e072f73641bef620dd820d13b74a
u1 <urban@xiala.net>
2013-12-29 00:59:22 +0100

    added Region-filtering in location-list

--- client/views/locations/locations.list.js
+++ client/views/locations/locations.list.js
@@ -1,17 +1,27 @@
 Template.locationlist.locations=function(){
-	var locations=Locations.find({});
+	var region = Session.get('region')
+	var locations = Locations.find({region: region});
 	for(m = 0; m < locations.count(); m++){
-		loc=locations.db_objects[m];
-		if(loc.hosts.contact.indexOf(Meteor.userId())!=-1){
-			loc.ismylocation="true";
+		loc = locations.db_objects[m];
+		if(loc.hosts.contact.indexOf(Meteor.userId()) != -1){
+			loc.ismylocation = "true";
 		}
 	}
 	return locations;
 }
 
+Template.locationlist.region=function(){
+	var regionId = Session.get('region')
+	var regionObj = Regions.findOne(regionId)
+	var regionName = regionObj ? regionObj.name : 'all regions'
+	return regionName
+}
+
+
+
 Template.location.events({
 	'click input.edit_location': function () {
-		alert('Work in progress, comming soon...');
+		alert('Work in progress, comming soon...');    // TODO!
 		Session.set("edit_location", this._id);
 	}
 });
