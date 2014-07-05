db3853bfe6c4b7df7f27f81a57f5b97ed5661efe
Stephan Balmer <sb@cis.ch>
2014-07-05 21:50:08 +0200

    Don't fail when course is not loaded yet

--- client/views/courses/details/course.events.js
+++ client/views/courses/details/course.events.js
@@ -1,5 +1,6 @@
 Template.course_events.events_list = function() {
-    var course=this.course;
+	var course=this.course;
+	if (!course) return [];
     var current_event=this.current_event;
 	var today = new Date();
 	return Events.find({course_id:course._id,startdate: {$gt:today}},{sort: {startdate: 1}}).map(function(event){
@@ -16,6 +17,7 @@ Template.course_events.events_list = function() {
 
 Template.course_events.events_list_past = function() {
     var course=this.course;
+	if (!course) return [];
     var current_event=this.current_event;
 	var today = new Date();
 	return Events.find({course_id:course._id,startdate: {$lt:today}},{sort: {startdate: -1}}).map(function(event){
