867c46020b9eb43c5df97f4bfc1a255fe6d8d415
Stephan Balmer <sb@cis.ch>
2014-07-06 03:31:26 +0200

    extracted translation strings

---
+++ server/mfExtracts.js
@@ -0,0 +1,124 @@
+mfPkg.addNative(
+{
+  "menue.Courses": {
+    "key": "menue.Courses",
+    "text": "Courses",
+    "file": "./client/views/main.html",
+    "line": 40,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.Search": {
+    "key": "menue.Search",
+    "text": "Search",
+    "file": "./client/views/main.html",
+    "line": 43,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.LocationList": {
+    "key": "menue.LocationList",
+    "text": "Location List",
+    "file": "./client/views/main.html",
+    "line": 44,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.CategoryList": {
+    "key": "menue.CategoryList",
+    "text": "Category List",
+    "file": "./client/views/main.html",
+    "line": 45,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.BrowseCourses": {
+    "key": "menue.BrowseCourses",
+    "text": "Browse Courses",
+    "file": "./client/views/main.html",
+    "line": 46,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.ProposeCourse": {
+    "key": "menue.ProposeCourse",
+    "text": "Propose Course",
+    "file": "./client/views/main.html",
+    "line": 47,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.CreateEvent": {
+    "key": "menue.CreateEvent",
+    "text": "Create Event",
+    "file": "./client/views/main.html",
+    "line": 48,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.Calendar": {
+    "key": "menue.Calendar",
+    "text": "Calendar",
+    "file": "./client/views/main.html",
+    "line": 57,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.about": {
+    "key": "menue.about",
+    "text": "about",
+    "file": "./client/views/main.html",
+    "line": 69,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.FAQ": {
+    "key": "menue.FAQ",
+    "text": "FAQ",
+    "file": "./client/views/main.html",
+    "line": 70,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.All regions": {
+    "key": "menue.All regions",
+    "text": "All regions",
+    "file": "./client/views/main.html",
+    "line": 85,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.Testpage": {
+    "key": "menue.Testpage",
+    "text": "This is only a <b>Testpage</b>. ",
+    "file": "./client/views/main.html",
+    "line": 101,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  },
+  "menue.Testpage2": {
+    "key": "menue.Testpage2",
+    "text": "All data will be erased regularly. ",
+    "file": "./client/views/main.html",
+    "line": 102,
+    "template": "layout",
+    "mtime": 1404604221980,
+    "ctime": 1404604221980
+  }
+}, 
+{
+  "extractedAt": 1404604221980,
+  "updatedAt": 1404604221980
+});
