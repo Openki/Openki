d6348956343bb188eddd9f2393fbf5e47c84bdb6
Stephan Balmer <sb@cis.ch>
2014-10-14 19:11:18 +0200

    Dschörmen translations

--- server/mfExtracts.js
+++ server/mfExtracts.js
@@ -4,121 +4,121 @@ mfPkg.addNative(
     "key": "menue.Courses",
     "text": "Courses",
     "file": "./client/views/main.html",
-    "line": 40,
+    "line": 44,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.Search": {
     "key": "menue.Search",
     "text": "Search",
     "file": "./client/views/main.html",
-    "line": 43,
+    "line": 47,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.LocationList": {
     "key": "menue.LocationList",
     "text": "Location List",
     "file": "./client/views/main.html",
-    "line": 44,
+    "line": 48,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.CategoryList": {
     "key": "menue.CategoryList",
     "text": "Category List",
     "file": "./client/views/main.html",
-    "line": 45,
+    "line": 49,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.BrowseCourses": {
     "key": "menue.BrowseCourses",
     "text": "Browse Courses",
     "file": "./client/views/main.html",
-    "line": 46,
+    "line": 50,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.ProposeCourse": {
     "key": "menue.ProposeCourse",
     "text": "Propose Course",
     "file": "./client/views/main.html",
-    "line": 47,
+    "line": 51,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.CreateEvent": {
     "key": "menue.CreateEvent",
     "text": "Create Event",
     "file": "./client/views/main.html",
-    "line": 48,
+    "line": 52,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.Calendar": {
     "key": "menue.Calendar",
     "text": "Calendar",
     "file": "./client/views/main.html",
-    "line": 57,
+    "line": 61,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.about": {
     "key": "menue.about",
     "text": "about",
     "file": "./client/views/main.html",
-    "line": 69,
+    "line": 73,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.FAQ": {
     "key": "menue.FAQ",
     "text": "FAQ",
     "file": "./client/views/main.html",
-    "line": 70,
+    "line": 74,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.All regions": {
     "key": "menue.All regions",
     "text": "All regions",
     "file": "./client/views/main.html",
-    "line": 85,
+    "line": 88,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.Testpage": {
     "key": "menue.Testpage",
     "text": "This is only a <b>Testpage</b>. ",
     "file": "./client/views/main.html",
-    "line": 101,
+    "line": 118,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   },
   "menue.Testpage2": {
     "key": "menue.Testpage2",
     "text": "All data will be erased regularly. ",
     "file": "./client/views/main.html",
-    "line": 102,
+    "line": 119,
     "template": "layout",
-    "mtime": 1404604221980,
-    "ctime": 1404604221980
+    "ctime": 1404604221980,
+    "mtime": 1404604221980
   }
 }, 
 {
-  "extractedAt": 1404604221980,
+  "extractedAt": 1413305835297,
   "updatedAt": 1404604221980
 });
