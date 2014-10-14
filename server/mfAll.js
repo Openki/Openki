55563bc9c3a9d6c67d249f856ddf68fbf20aa9ed
Stephan Balmer <sb@cis.ch>
2014-10-14 19:13:57 +0200

    Dschörmen translations
    The real schit

---
+++ server/mfAll.js
@@ -0,0 +1,282 @@
+mfPkg.syncAll({
+  "en": {
+    "menue.Courses": {
+      "key": "menue.Courses",
+      "lang": "en",
+      "text": "Courses",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "s7HffK52QMJWhRfZb",
+      "file": "./client/views/main.html",
+      "line": 44,
+      "template": "layout",
+      "_id": "uk9i4nysDdFkv424h"
+    },
+    "menue.Search": {
+      "key": "menue.Search",
+      "lang": "en",
+      "text": "Search",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "4XnKr2YdFqw5Hfpqy",
+      "file": "./client/views/main.html",
+      "line": 47,
+      "template": "layout",
+      "_id": "pBDHnC7MExQseJGuy"
+    },
+    "menue.LocationList": {
+      "key": "menue.LocationList",
+      "lang": "en",
+      "text": "Location List",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "6cnxijxNNckqpKsda",
+      "file": "./client/views/main.html",
+      "line": 48,
+      "template": "layout",
+      "_id": "XtJkz8ZcTFgs6J8ve"
+    },
+    "menue.CategoryList": {
+      "key": "menue.CategoryList",
+      "lang": "en",
+      "text": "Category List",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "oTBME7AiHjJH4kvCt",
+      "file": "./client/views/main.html",
+      "line": 49,
+      "template": "layout",
+      "_id": "Tpojz5iLaDArrvgWp"
+    },
+    "menue.BrowseCourses": {
+      "key": "menue.BrowseCourses",
+      "lang": "en",
+      "text": "Browse Courses",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "gyne3AvXnQjMfxGv7",
+      "file": "./client/views/main.html",
+      "line": 50,
+      "template": "layout",
+      "_id": "mf47qrosf4f8ec8C2"
+    },
+    "menue.ProposeCourse": {
+      "key": "menue.ProposeCourse",
+      "lang": "en",
+      "text": "Propose Course",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "68zwZdtfWzSmJqcsr",
+      "file": "./client/views/main.html",
+      "line": 51,
+      "template": "layout",
+      "_id": "vyzxxKbMfEvCsHGZC"
+    },
+    "menue.CreateEvent": {
+      "key": "menue.CreateEvent",
+      "lang": "en",
+      "text": "Create Event",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "jCyZNtNeSAwuTCEiW",
+      "file": "./client/views/main.html",
+      "line": 52,
+      "template": "layout",
+      "_id": "oNqqrDroAYWfC4C5M"
+    },
+    "menue.Calendar": {
+      "key": "menue.Calendar",
+      "lang": "en",
+      "text": "Calendar",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "k2z8m24hhQ6gmdCCa",
+      "file": "./client/views/main.html",
+      "line": 61,
+      "template": "layout",
+      "_id": "gcvNfnvBCgjfKCpBM"
+    },
+    "menue.about": {
+      "key": "menue.about",
+      "lang": "en",
+      "text": "about",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "MwDTMzrQ3LySqA2sd",
+      "file": "./client/views/main.html",
+      "line": 73,
+      "template": "layout",
+      "_id": "pFTiFSA2ucPcNi4e3"
+    },
+    "menue.FAQ": {
+      "key": "menue.FAQ",
+      "lang": "en",
+      "text": "FAQ",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "yLt2YirW4L5d7naYv",
+      "file": "./client/views/main.html",
+      "line": 74,
+      "template": "layout",
+      "_id": "NEhwjta5kbda7zKQG"
+    },
+    "menue.All regions": {
+      "key": "menue.All regions",
+      "lang": "en",
+      "text": "All regions",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "sp92NYYZsBRNZzXaL",
+      "file": "./client/views/main.html",
+      "line": 88,
+      "template": "layout",
+      "_id": "EftP3c7miK6YGWQEy"
+    },
+    "menue.Testpage": {
+      "key": "menue.Testpage",
+      "lang": "en",
+      "text": "This is only a <b>Testpage</b>. ",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "5nJNxsHzN6x9dkHGp",
+      "file": "./client/views/main.html",
+      "line": 118,
+      "template": "layout",
+      "_id": "Kqot64HdHSY4BM5tL"
+    },
+    "menue.Testpage2": {
+      "key": "menue.Testpage2",
+      "lang": "en",
+      "text": "All data will be erased regularly. ",
+      "ctime": 1404604221980,
+      "mtime": 1404604221980,
+      "revisionId": "BctEsz65w7xmpidae",
+      "file": "./client/views/main.html",
+      "line": 119,
+      "template": "layout",
+      "_id": "M3GQ9AzEb63Z5iGoS"
+    }
+  },
+  "de": {
+    "menue.Courses": {
+      "key": "menue.Courses",
+      "lang": "de",
+      "text": "Kurse",
+      "ctime": 1413306270570,
+      "mtime": 1413306270570,
+      "revisionId": "9uECmZHcy5otSf3hG",
+      "_id": "EoKkNA4wCaqkYtnAQ"
+    },
+    "menue.Search": {
+      "key": "menue.Search",
+      "lang": "de",
+      "text": "Suche",
+      "ctime": 1413306297614,
+      "mtime": 1413306297614,
+      "revisionId": "dWPn9nWQrR5GhxJWz",
+      "_id": "zt3ujibdain4K9inc"
+    },
+    "menue.LocationList": {
+      "key": "menue.LocationList",
+      "lang": "de",
+      "text": "Kursorte",
+      "ctime": 1413306310062,
+      "mtime": 1413306310062,
+      "revisionId": "gEwg9hLxSegXejZHZ",
+      "_id": "9nNng8EYgZsj2mbSf"
+    },
+    "menue.CategoryList": {
+      "key": "menue.CategoryList",
+      "lang": "de",
+      "text": "Liste der Kategorien",
+      "ctime": 1413306323156,
+      "mtime": 1413306323156,
+      "revisionId": "tTtRn3PQa7NH6EKRN",
+      "_id": "SbWTPM9xLv2D9yvkx"
+    },
+    "menue.BrowseCourses": {
+      "key": "menue.BrowseCourses",
+      "lang": "de",
+      "text": "Kurse abgrasen",
+      "ctime": 1413306340764,
+      "mtime": 1413306340764,
+      "revisionId": "CsSr4SBoPWZ82Govu",
+      "_id": "Mt88WyMeXQLkgMm6Y"
+    },
+    "menue.ProposeCourse": {
+      "key": "menue.ProposeCourse",
+      "lang": "de",
+      "text": "Kurs vorschlagen",
+      "ctime": 1413306347109,
+      "mtime": 1413306347109,
+      "revisionId": "MwPgRcSCaksiLs6zw",
+      "_id": "BHsfMgHxKCWCP5pba"
+    },
+    "menue.CreateEvent": {
+      "key": "menue.CreateEvent",
+      "lang": "de",
+      "text": "Abmachen",
+      "ctime": 1413306366317,
+      "mtime": 1413306366317,
+      "revisionId": "mMZkvK3WRQwcdnJP5",
+      "_id": "dxJvJEQYdiWbWzXs3"
+    },
+    "menue.Calendar": {
+      "key": "menue.Calendar",
+      "lang": "de",
+      "text": "Kalender",
+      "ctime": 1413306370620,
+      "mtime": 1413306370620,
+      "revisionId": "SFRemQEKzMRH6nWQa",
+      "_id": "MAfa8rq2fhsMiByMP"
+    },
+    "menue.about": {
+      "key": "menue.about",
+      "lang": "de",
+      "text": "über",
+      "ctime": 1413306374544,
+      "mtime": 1413306374544,
+      "revisionId": "QakMoj9nP2E9P46bS",
+      "_id": "qmEuFsHaSfGauw8eB"
+    },
+    "menue.FAQ": {
+      "key": "menue.FAQ",
+      "lang": "de",
+      "text": "Fragen und Antworten",
+      "ctime": 1413306389962,
+      "mtime": 1413306389962,
+      "revisionId": "nsXnwxAdzaH5xWGQo",
+      "_id": "Mz5zZmsTW3bBQc6y2"
+    },
+    "menue.All regions": {
+      "key": "menue.All regions",
+      "lang": "de",
+      "text": "Alle Regionen",
+      "ctime": 1413306393950,
+      "mtime": 1413306393950,
+      "revisionId": "QikuyFDtxPmdPm5Nf",
+      "_id": "ZXruLqX2SdyJFdjyu"
+    },
+    "menue.Testpage": {
+      "key": "menue.Testpage",
+      "lang": "de",
+      "text": "Das ist <b>nur eine Testseite</b>",
+      "ctime": 1413306413941,
+      "mtime": 1413306413941,
+      "revisionId": "exbLiypEKFzvEc6dH",
+      "_id": "awzyTwbbBALpxdoAC"
+    },
+    "menue.Testpage2": {
+      "key": "menue.Testpage2",
+      "lang": "de",
+      "text": "Wird alles regelmässig gelöscht",
+      "ctime": 1413306473512,
+      "mtime": 1413306473512,
+      "revisionId": "59bWtP7eAHa6n9LeE",
+      "_id": "XY68Rkb8bG89g4Z49"
+    }
+  }
+}, {
+  "exportedAt": 1413306485103,
+  "updatedAt": 1413306473512
+});
