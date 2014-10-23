7dcd397bed79a1e224293e43bf78c96df1f5912e
Stephan Balmer <sb@cis.ch>
2014-10-23 13:21:05 +0200

    preparation

--- client/views/login/login_new.js
+++
@@ -1,8 +0,0 @@
-Template.layout.rendered=function(){
-	if(!Meteor.user()){
-		$('.login-link-text').text("Sign Up/Sign In");
-	}else{
-		$('#login-buttons-logout').before('<a class="profile-link-button" href="/user/'+Meteor.user()._id+'">My Public Profile</a><br/>');
-		$('#login-buttons-logout').before('<a class="profile-link-button" href="/profile">My Account</a>');
-  }
-};
