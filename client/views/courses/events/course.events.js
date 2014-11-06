c1e3343ce6d1010aa9829a645470ff441b205ada
Stephan Balmer <sb@cis.ch>
2014-11-06 15:38:25 +0100

    event fixes
    to be continued

--- client/views/courses/events/course.events.js
+++ client/views/courses/events/course.events.js
@@ -1,40 +1,47 @@
-Template.course_events.events_list = function() {
-	var course=this.course;
-	if (!course) return [];
-    var current_event=this.current_event;
-	var today = new Date();
-	return Events.find({course_id:course._id,startdate: {$gt:today}},{sort: {startdate: 1}}).map(function(event){
-		var isCurrent = false;
-		if(current_event && current_event._id==event._id) isCurrent=true;
-		return {
-			course: course, 
-			event: event, 
-			isCurrent: isCurrent
-		}
-	});
+
+var editingEvent = function (event) {
+	return Router.current().params.editEvent === event
 };
 
-
-Template.course_events.events_list_past = function() {
-    var course=this.course;
-	if (!course) return [];
-    var current_event=this.current_event;
-	var today = new Date();
-	return Events.find({course_id:course._id,startdate: {$lt:today}},{sort: {startdate: -1}}).map(function(event){
-		var isCurrent = false;
-		if(current_event && current_event._id==event._id) isCurrent=true;
-		return {
-			course: course, 
-			event: event, 
-			isCurrent: isCurrent
-		}
-	});
-};
-
-
-Template.course_events.new_event = function() {
-	return {course: this.course, event: {}}
-}
+Template.course_events.helpers({
+	events_list: function() {
+		var course=this.course;
+		if (!course) return [];
+		var current_event=this.current_event;
+		var today = new Date();
+		return Events.find({course_id:course._id,startdate: {$gt:today}},{sort: {startdate: 1}}).map(function(event){
+			var isCurrent = false;
+			if(current_event && current_event._id==event._id) isCurrent=true;
+			return {
+				course: course, 
+				event: event, 
+				isCurrent: isCurrent
+			}
+		});
+	},
+	
+	events_list_past: function() {
+		var course=this.course;
+		if (!course) return [];
+		var current_event=this.current_event;
+		var today = new Date();
+		return Events.find({course_id:course._id,startdate: {$lt:today}},{sort: {startdate: -1}}).map(function(event){
+			var isCurrent = false;
+			if(current_event && current_event._id==event._id) isCurrent=true;
+			return {
+				course: course, 
+				event: event, 
+				isCurrent: isCurrent
+			}
+		});
+	},
+	
+	new_event: function() {
+		return {course: this.course, event: {}}
+	},
+	
+	editingEvent: editingEvent
+});
 
 
 Template.course_events.events({
@@ -63,12 +70,10 @@ Template.course_events.events({
 		}
 		Router.go(Router.current().route.name, Router.current().params, { query: { 'editEvent': this.event._id } }) // Shirely, you know of a better way?
 	}
-})
+});
 
-Template.course_event.editingEvent = function (event) {
-	return Router.current().params.editEvent === event
-};
-Template.course_events.editingEvent = function (event) {
-	return Router.current().params.editEvent === event
-};
+
+Template.course_event.helpers({
+	editingEvent: editingEvent
+});
 
