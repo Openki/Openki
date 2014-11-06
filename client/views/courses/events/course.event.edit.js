439ce975cbf04d20acb029549e3dbd4d6f27e1d2
Stephan Balmer <sb@cis.ch>
2014-11-06 15:59:31 +0100

    fix errors in event detail
    at some point we need to figure out how to properly wait till our data is loaded

--- client/views/courses/events/course.event.edit.js
+++ client/views/courses/events/course.event.edit.js
@@ -63,6 +63,7 @@ Template.course_event_edit.events({
 
 Template.course_event_edit.helpers({
 	needsRole: function(role) {
+		if (!this.course) return false;
 		return this.course.roles.indexOf(role) != -1
 	},
 
@@ -94,21 +95,22 @@ Template.course_event_edit.helpers({
 
 	possible_hosts: function() {
 		var possible_hosts=[]
-		var hostList = havingRole(this.course.members, 'host')
-		var eventobj=this.event;
-		_.each(hostList, function (userid) {
-			var user = Meteor.users.findOne({_id: userid})
-			if (!user) return;
-			if(eventobj._id){
-				if(eventobj.host!=user._id){
-					var checked ="";
-				}else{
-					var checked ="checked";
-				};
-			}
-			possible_hosts.push({"username": user.username, "id":user._id, "checked":checked})
-		});
-
+		if (this.course) {
+			var hostList = havingRole(this.course.members, 'host')
+			var eventobj=this.event;
+			_.each(hostList, function (userid) {
+				var user = Meteor.users.findOne({_id: userid})
+				if (!user) return;
+				if(eventobj._id){
+					if(eventobj.host!=user._id){
+						var checked ="";
+					}else{
+						var checked ="checked";
+					};
+				}
+				possible_hosts.push({"username": user.username, "id":user._id, "checked":checked})
+			});
+		}
 		return possible_hosts;
 	}
 });
