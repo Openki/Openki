13d3b25100c84602fdb3dd8234a3462dd8acb42f
u1 <urban@xiala.net>
2014-11-25 21:31:08 +0100

    disabling roles "interested" and "follow"

--- collections/roles.js
+++ collections/roles.js
@@ -15,24 +15,21 @@ var roles = [{
 		'type':        'team',
 		'description': 'look after the course',
 		'subscribe':   'I help organize',
-		'preset':      true,
+		'preset':      true,					//if true: allways available, not offered as choice in proposal
 		'show_subscribers': true,
-		'protorole': { 'subscribed': [] }
-	},{
+	},/*{
 		'type':        'interested',
 		'description': 'are interested',
 		'subscribe':   'I want to be notified when it starts',
 		'preset':      true,
 		'show_subscribers': false,
-		'protorole': { 'subscribed': [] }
 	},{
 		'type':        'follow',
 		'description': 'get informed',
 		'subscribe':   'I want to be notified about all changes' ,
 		'preset':      true,
 		'show_subscribers': false,
-		'protorole': { 'subscribed': [] }
-	},{
+	},*/{
 		'type':        'participant',
 		'description': 'take part',
 		'subscribe':   'I want to take part',
@@ -41,13 +38,11 @@ var roles = [{
 			'min': { 'type': 'int', 'description': 'minimal count of participants', 'optional': true },
 			'max': { 'type': 'int', 'description': 'maximal count of participants', 'optional': true },
 		},
-		'protorole': { 'subscribed': [] }
 	},{
 		'type':        'mentor',
 		'description': 'are a mentor',
 		'subscribe':   'I can be a mentor',
 		'show_subscribers': true,
-		'protorole': { 'subscribed': [] }
 	},{
 		'type':        'host',
 		'description': 'host the course',
@@ -56,7 +51,6 @@ var roles = [{
 		'fields': {
 			'address': 'text'
 		},
-		'protorole': { 'subscribed': [] }
 	},{
 		'type':        'donator',
 		'description': 'donate to the course',
@@ -65,13 +59,11 @@ var roles = [{
 		'fields': {
 			'what': 'text'
 		},
-		'protorole': { 'subscribed': [] }
 	},{
 		'type':        'cook',
 		'description': 'cook for gatherings',
 		'subscribe':   'I can bring food' ,
 		'show_subscribers': true,
-		'protorole': { 'subscribed': [] }
 	}
 ]
 
