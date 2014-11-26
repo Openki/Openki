00d9302cbda6f5a2f69d4c0c14cd1b2aadf6c95d
0rel <me@0rel.com>
2014-11-26 19:58:09 +0100

    preventDefault() just to be sure that the event is not used to follow the pseudo link...

--- client/views/region.js
+++ client/views/region.js
@@ -10,10 +10,11 @@ Template.region_sel.helpers({
 });
 
 Template.region_sel.events({
-	'click a.regionselect': function(){
+	'click a.regionselect': function(e){
 		var region_id = this._id ? this._id : 'all'
 		localStorage.setItem("region", region_id); // to survive page reload
 		Session.set('region', region_id)
 		//return false
+		e.preventDefault();
 	}
 })
