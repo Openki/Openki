51bca5626b44c681c762a6c92f64ffc0d7cbef2b
Stephan Balmer <sb@cis.ch>
2014-12-13 23:36:18 +0100

    avoid conflict with chat messages

--- client/messages/lib.js
+++ client/messages/lib.js
@@ -1,9 +1,9 @@
-Messages = new Meteor.Collection(null);
+ClientMessages = new Meteor.Collection(null);
 
 addMessage = function(message) {
-  Messages.insert({message: message})
+  ClientMessages.insert({message: message})
 }
 
 removeMessage = function(message) {
-  Messages.remove({_id: message._id})
+  ClientMessages.remove({_id: message._id})
 }
