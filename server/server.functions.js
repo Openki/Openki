619ec08d1c329f1fe434ba6d1b0a20b2a6dcd504
1u <urban@xiala.net>
2014-12-14 02:40:27 +0100

    Adding global config file

--- server/server.functions.js
+++ server/server.functions.js
@@ -1,12 +1,3 @@
-Meteor.startup(function () {
-	createGroupsIfNone();          // TESTING...
-	createCategoriesIfNone();
-	createCoursesIfNone();         // TESTING...
-	createLocationsIfNone();       // TESTING...
-	createEventsIfNone();          // TESTING...
-});
-
-
 Meteor.methods({
 	insert_userdata: function(username, email, password){
 		Accounts.createUser({username:username, email:email, password:password});
@@ -48,7 +39,7 @@ Meteor.methods({
 
 // SETUP: Create Categories if not all anymore
 
-function createCategoriesIfNone() {
+createCategoriesIfNone = function() {
 	if (Categories.find().count() === 0) {
 		_.each(categories, function(category){
 			Categories.insert(category)
