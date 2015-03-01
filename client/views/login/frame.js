823edcfba0d1fdbd8790b2b4aab30af3cfdca86a
1u <urban@xiala.net>
2015-03-01 04:15:44 +0100

    adding email.field for registering

--- client/views/login/frame.js
+++ client/views/login/frame.js
@@ -12,26 +12,43 @@ Template.loginFrame.helpers({
   }
 });
 
- 
+
+Template.loginLogin.created = function() {
+	this.registering = new ReactiveVar(false);
+}
+
 Template.loginLogin.events({
 	'click .loginRegister': function(event, template){
 		event.preventDefault();
-		var name = template.find('#login-name').value;
-		var password = template.find('#login-password').value;
-		console.log(name)
-		Accounts.createUser({
-			username: name,
-			password: password
-		}, function(err) {
-			if (err) {
-				addMessage(err);
-			} else {
-				Session.set('showLogin', false);
-			}
-		});
+		if(Template.instance().registering.get()){
+			var name = template.find('#login-name').value;
+			var password = template.find('#login-password').value;
+			var email = template.find('#login-email').value;
+			Accounts.createUser({
+				username: name,
+				password: password,
+				email: email
+			}, function(err) {
+				if (err) {
+					addMessage(err);
+				} else {
+					Session.set('showLogin', false);
+				}
+			});
+		}
+		else {
+			$('#show_email').show(300);
+			Template.instance().registering.set(true);
+		}
 	},
+
 	'submit form, click .loginLogin': function(event, template){
 		event.preventDefault();
+		if(Template.instance().registering.get()){
+			$('#show_email').hide(300);
+			Template.instance().registering.set(false);
+			return;
+		}
 		var name = template.find('#login-name').value;
 		var password = template.find('#login-password').value;
 		Meteor.loginWithPassword(name, password, function(err) {
