ec1fa66e8b534fadfbffa12f0a3b175381114241
sb <sb@cis.ch>
2015-03-05 01:17:43 +0100

    add query parameters count and lg
    typical url: http://openki.home/banner/events/MyHome?count=3&lg=zh_TW

--- client/views/misc/banner/banner.js
+++ client/views/misc/banner/banner.js
@@ -1,13 +1,14 @@
 Router.map(function () {
 	this.route('banner', {
-		path: '/banner/events/:location?/:room?/:limit?',
+		path: '/banner/events/:location?/:room?',
 		template: 'bannerEvents',
 		layoutTemplate: 'bannerLayout',
 		waitOn: function () {
 			Session.get('roughTime'); // Time dependency so this will be reactively updated
 			// The haphazard type mangling is due to moment() being reactive when initialized with current date (really??) so converting the current date to a string then reading that was the quickest workaround I could think of :-D
 			var endtime = moment(''+new Date()).toDate();
-			var limit = this.params.limit*1 || 5           //FIXME  how to remove "*1"
+
+			var limit = parseInt(this.params.query.count, 10) || 5;
 			if (this.params.location == 'All') this.params.location = undefined;
 			if (this.params.room == 'All') this.params.room = undefined;
 			return Meteor.subscribe('eventsFind', endtime, limit*2, this.params.location, this.params.room);
@@ -20,13 +21,16 @@ Router.map(function () {
 			Session.get('fineTime');
 			// REVIEW we always do the same things, subscribing in waitOn() then again find() in data().
 			var endtime = moment(''+new Date()).toDate();
-			var limit = this.params.limit*1 || 5           //FIXME  how to remove "*1"
+			var limit = parseInt(this.params.query.count, 10) || 5;
 			if (this.params.location == 'All') this.params.location = undefined;
 			if (this.params.room == 'All') this.params.room = undefined;
 			return eventsFind(endtime, limit, this.params.location, this.params.room);
 		},
 		onAfterAction: function() {
-			document.title = webpagename + ' Events'
+			document.title = webpagename + ' Events';
+			if (this.params.query.lg) {
+				Session.set('locale', this.params.query.lg);
+			}
 		}
 	});
 });
