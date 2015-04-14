68a4a6288133518b9024003dcef839dd925cd32d
Marco <macrozone@gmail.com>
2015-04-14 23:30:51 +0200

    added live search

--- client/views/find/find.js
+++ client/views/find/find.js
@@ -2,7 +2,7 @@ Router.map(function () {
 	this.route('find', {
 		path: '/find/:search?',
 		template: 'find',
-		waitOn: function () {
+		subscriptions: function () {
 			var region = Session.get('region')
 			var filter = {}
 			filter.hasUpcomingEvent = !!this.params.query.hasUpcomingEvent;
@@ -44,6 +44,7 @@ var submitForm = function(event) {
 Template.find.events({
 	'submit': submitForm,
 	'change .search': submitForm,
+	'keyup .search': _.debounce(submitForm, 300),
 	//TEMPORARY EVENTS FOR NACHHALTIGKEITSWOCHE HEADER
 	'click button.nw_close': function() { 
 		Session.set('showHeader', "hideIt");
