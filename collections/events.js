e28572eb958b6405ac257c79603b2f7d870e93b8
Emre Sarigol <emresarigol@gmail.com>
2015-04-23 19:25:08 +0200

    event period selection v1

--- collections/events.js
+++ collections/events.js
@@ -204,40 +204,5 @@ eventsFind = function(filter, limit) {
 
 
 
-var fileStore = new FS.Store.GridFS("files");
-
-Files = new FS.Collection("files", {
-	stores: [fileStore]
-});
-
-Files.deny({
-	insert: function(){
-		return false;
-	},
-	update: function(){
-		return false;
-	},
-	remove: function(){
-		return false;
-	},
-	download: function(){
-		return false;
-	}
- });
-
-Files.allow({
-	insert: function(){
-		return true;
-	},
-	update: function(){
-		return true;
-	},
-	remove: function(){
-		return true;
-	},
-	download: function(){
-		return true;
-	}
-});
 
 
