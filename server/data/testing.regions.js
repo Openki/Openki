e801e634384dd9716725345345a7e2f0902e6769
1u <urban@xiala.net>
2015-05-01 03:10:07 +0200

    adding region Frauenfeld, adding Country- and region-code to all regions aswell as long-/lat-data

--- server/data/testing.regions.js
+++ server/data/testing.regions.js
@@ -13,17 +13,41 @@ regions = [{
 	},{
 		'name':        'Bern',
 		'_id':         'Siifr2P7drkv66FNA',
+		'country':     'CH',
+		'region':      'BE', 
+		'latitude':    '46.9511',
+		'longitude':   '7.4386',
 		'timeZone':    'UTC+01:00'
 	},{
 		'name':        'Zürich',
 		'_id':         'J6GDhEEvdmdSMzPPF',
+		'country':     'CH',
+		'region':      'ZH',
+		'latitude':    '47.3784',
+		'longitude':   '8.5257',
+		'timeZone':    'UTC+01:00'
+	},{
+		'name':        'Frauenfeld',
+		'_id':         'doR7JQH0jDE7Uunp3',
+		'country':     'CH',
+		'region':      'TG',
+		'latitude':    '47.556',
+		'longitude':   '8.8965',
 		'timeZone':    'UTC+01:00'
 	},{
 		'name':        'Biel',
+		'country':     'CH',
+		'region':      'BE',
+		'latitude':    '47.1375',
+		'longitude':   '7.2472',
 		'_id':         'Gkkr8Deb2ln4hGp7O',
 		'timeZone':    'UTC+01:00'
 	},{
 		'name':        'Luzern',
+		'country':     'CH',
+		'region':      'LU',
+		'latitude':    '47.05',
+		'longitude':   '8.3',
 		'_id':         'h6ZeeIBfp72msnnp3',
 		'timeZone':    'UTC+01:00'
 	}
