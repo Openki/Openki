614a1c27d4a29c67dc1b5857de1cb2c3feac7d3e
sb <sb@cis.ch>
2015-05-13 04:23:35 +0200

    housekeeping

--- config.js
+++ config.js
@@ -15,8 +15,6 @@ Meteor.startup(function () {
 ScaleFaktor = 1;
 
 // Uncomment the following lines if you want to have testdata loaded 
-// by the script in server/testing.createnload.data.js 
-// (there are probably some dependencies) -> FIXME
 
 	createGroupsIfNone();          // Groups     from server/data/testing.groups.js
 	createTestRegionsIfNone();     // Regions    from server/data/testing.regions.js  
@@ -26,33 +24,4 @@ ScaleFaktor = 1;
 	loadTestEvents();              // Events     from server/data/testing.events.js
 
 
-/////////////  upcomming:  ///////////////
-// loadTestComments();          FIXME/TODO
-// loadTemporaryEvent-Setting(Region=Zurich);  FIXME/TODO
-//
-// enable to show testing environment and prototype-messages
-// TEST-ENV=1                   FIXME/TODO
-//
-// load critical-content-courses
-// Critical=1                   FIXME/TODO
-//
-// enable social login buttons  FIXME/TODO
-// Github                       FIXME/TODO
-// Facebook                     FIXME/TODO
-//////////////////////////////////////////
-
-
 })}
-
-
-this.AdminConfig = {
-	name: 'Опеньки',
-	adminEmails: ['greg@openki.example'],
-	collections: {
-		Courses: {
-			tableColumns: [
-				{label: 'Name', name: 'name'}
-			]
-		}
-	}
-};
