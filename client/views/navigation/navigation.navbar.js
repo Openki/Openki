56fb8ae75acda3e7c6c5bce63ddf569b3e665336
lu40 <lucaobertuefer@mailbox.org>
2015-06-18 15:44:14 +0200

    add new dropdown and active page style

--- client/views/navigation/navigation.navbar.js
+++ client/views/navigation/navigation.navbar.js
@@ -1,5 +1,11 @@
 Template.navbar.helpers ({
     kioskMode: function() {
         return Session.get('kiosk_mode');
+    },
+
+    checkActiveRoute: function() {
+        var currentRoute = Router.current().route
+        this.$('a[href="' + currentRoute.path(this) + '"].nav_link').addClass('active');
+        this.$('a[href!="' + currentRoute.path(this) + '"].nav_link').removeClass('active');
     }
-})
+});
