3db760d314f0ab4c5c79fe300440a1f745254ff3
lu40 <lucaobertuefer@mailbox.org>
2015-06-24 02:43:32 +0200

    change visual hint of active nav-link when navbar is above content Changes from caret to highlighted background when scrolling down a little

--- client/views/navigation/navigation.navbar.js
+++ client/views/navigation/navigation.navbar.js
@@ -12,4 +12,13 @@ Template.navbar.rendered = function() {
     this.$('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
+    $(window).scroll(function (event) {
+        if($(window).scrollTop() > 5){
+            this.$('a.nav_link.active').addClass('over_content');
+        }
+        else if($(window).scrollTop() < 5){
+            this.$('a.nav_link.active').removeClass('over_content');            
+        }
+    });
 };
+
