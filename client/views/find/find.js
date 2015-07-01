31400d0f236d2980d585a223ae8c4689462fd040
sb <sb@cis.ch>
2015-07-01 19:13:45 +0200

    make search smoother
    very ugly, but an improvement

--- client/views/find/find.js
+++ client/views/find/find.js
@@ -10,31 +10,8 @@ function finderRoute(path) {
 			};
 			this.next();
 		},
-		subscriptions: function () {
-			var region = Session.get('region')
-			var filter = {}
-			filter.hasUpcomingEvent = !!this.params.query.hasUpcomingEvent;
-			if (this.params.query.category) {
-				filter.categories = this.params.query.category.split(',');
-			}
-			return [
-				Meteor.subscribe('coursesFind', region, this.params.search, filter, 36),
-				Meteor.subscribe('eventsFind', { query: this.params.search, standalone:true, region: region }, 10)
-			];
-		},
 		data: function() {
-			var region = Session.get('region')
-			var filter = {};
-			filter.hasUpcomingEvent = !!this.params.query.hasUpcomingEvent;
-			if (this.params.query.category) {
-				filter.categories = this.params.query.category.split(',');
-			}
-			return {
-				filter: filter,
-				query: this.params.search,
-				results: coursesFind(region, this.params.search, filter, 36),
-				eventResults: eventsFind({ query: this.params.search, standalone: true }, 10)
-			}
+			return this.params.query;
 		},
 		onAfterAction: function() {
 			document.title = webpagename + 'Find ' + this.params.search
@@ -47,59 +24,76 @@ Router.map(function () {
 	this.route('home', finderRoute('/'));
 });
 
-var submitForm = function(event, template) {
-	options = {}
-	
+var searchChanged = function(event, instance) {
 	var queryParams = [];
-	if ($("#hasUpcomingEvent")[0].checked) {
+
+	instance.hasUpcomingEvent.set(!!$("#hasUpcomingEvent")[0].checked);
+	if (instance.hasUpcomingEvent.get()) {
 		queryParams.push("hasUpcomingEvent");
 	}
-	if (template.data.filter.categories) {
-		queryParams.push('category='+template.data.filter.categories.join(','));
+
+	var cats = instance.categories.get();
+	if (cats.length) {
+		queryParams.push('category='+cats.join(','));
 	}
 
+	options = {}
 	if (queryParams.length) {
 		options.query = queryParams.join('&');
 	}
 
-	Router.go('find', { search: $('#find').val().replace("/", " ")}, options )
+	Router.go('find', { search: $('.searchInput').val().replace("/", " ")}, options);
 	event.preventDefault();
-	event.stopPropagation();
-	return false; 
 }
 
 Template.find.events({
-	'submit': submitForm,
-	'change .search': submitForm,
-	'keyup .search': _.debounce(submitForm, 300),
-	
 	'click button.readmore': function() {
 		if (Session.get('showInfo') != true) {
 			Session.set('showInfo', true);
 		}
 		else Session.set('showInfo', false);
 	},
+	'submit': searchChanged,
+	'change .search': searchChanged,
+	'keyup .searchInput': _.debounce(function(event, instance) {
+		instance.search.set($('.searchInput').val());
+	}, 300),
 	
-	'click .category': function(event, template) {
-		var cats = template.data.filter.categories || [];
+	'click .category': function(event, instance) {
+		var cats = instance.categories.get();
 		cats.push(""+this);
-		template.data.filter.categories = _.uniq(cats);
-		submitForm(event, template);
+		instance.categories.set(_.uniq(cats));
+		searchChanged(event, instance);
 		return false;
 	},
 
-	'click .removeCategoryFilter': function(event, template) {
-		var cats = template.data.filter.categories || [];
+	'click .removeCategoryFilter': function(event, instance) {
+		var cats = instance.categories.get();
 		var remCat = ''+this; // comes in a s string object, coerce to string
-		template.data.filter.categories = _.without(cats, remCat);
-		submitForm(event, template);
+		instance.categories.set(_.without(cats, remCat));
+		submitForm(event, instance);
 		return false;
 	}
 });
 
+var readFilter = function(instance) {
+	var filter = {};
+	if (instance.hasUpcomingEvent.get()) {
+		filter.hasUpcomingEvent = true;
+	}
+
+	var categories = instance.categories.get();
+	if (categories.length) {
+		filter.categories = categories;
+	}
+
+	return filter;
+}
+
 Template.find.helpers({
 	'hasUpcomingEventsChecked': function() {
-		if (this.filter.hasUpcomingEvent) return "checked";
+		if (Template.instance().hasUpcomingEvent.get()) return "checked";
+		return "";
 	},
 	
 	'newCourse': function() {
@@ -108,6 +102,63 @@ Template.find.helpers({
 			region: Session.get('region')
 		}
 	},
+
+	'categories': function() {
+		return Template.instance().categories.get();
+	},
+
+	'results': function() {
+		var instance = Template.instance();
+
+		// Wonky: We should use readFilter() here, like the template
+		// subscriptions do. But if we do that the hasUpcomingEvents filter
+		// will exclude all courses because they don't have their event loaded
+		// yet, their template will do that.
+		var filter = {};
+
+		return coursesFind(Session.get('region'), instance.search.get(), filter, 36);
+	},
+
+	'eventResults': function() {
+		return eventsFind({ query: Template.instance().search.get(), standalone: true }, 10);
+	},
+
+	'ready': function() {
+		var instance = Template.instance();
+		return instance.subs[0] && instance.subs[0][0].ready() && instance.subs[0][1].ready();
+	}
+});
+
+Template.find.onCreated(function() {
+	var instance = this;
+
+	var search = instance.data.search ? instance.data.search : '';
+	instance.search = new ReactiveVar(search);
+
+	var hasUpcomingEvent = !!instance.data.hasUpcomingEvent;
+	instance.hasUpcomingEvent = new ReactiveVar(hasUpcomingEvent);
+
+
+	var categories = instance.data.category ? instance.data.category.split(',') : [];
+	instance.categories = new ReactiveVar(categories);
+
+	// Keep two sets of course/event subscriptions around.
+	// This way, the search results don't blink out and back when we switch
+	// subscriptions
+	instance.subs = [];
+
+	instance.autorun(function() {
+		var search = instance.search.get();
+		var region = Session.get('region');
+
+		// Stop the penultimate subscriptions
+		if (instance.subs.length > 1) _.map(instance.subs.pop(), function(sub) { sub.stop() });
+
+		instance.subs.unshift([
+			instance.subscribe('coursesFind', region, search, readFilter(instance), 36),
+			instance.subscribe('eventsFind', { query: search, standalone: true, region: region }, 10)
+		]);
+	});
 });
 
 Template.find.rendered = function() {
