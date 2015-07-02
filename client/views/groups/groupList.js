5766bd6fce203c02301219f93f5b0fa7ea15020a
sb <sb@cis.ch>
2015-07-02 02:37:59 +0200

    list groups like categories

---
+++ client/views/groups/groupList.js
@@ -0,0 +1,9 @@
+Template.groupList.helpers({
+	name: function() {
+		if (!this) return;
+		var groupId = ''+this; // it's not a string?! LOL I DUNNO
+		Template.instance().subscribe('group', groupId);
+		var group = Groups.findOne(groupId);
+		return group && group.name;
+	}
+});
