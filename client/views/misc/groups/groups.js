0be3464c33295ce8f7e06eea49344d24a254ed16
1u <urban@xiala.net>
2015-09-10 01:13:35 +0200

    removing helper show_group and two files by replacing with groupList

--- client/views/misc/groups/groups.js
+++
@@ -1,6 +0,0 @@
-Template.show_groups.helpers({
-	'loadgroups': function(groups) {
-		if (!groups) return [];                           // IGNORANCE IS STRENGTH
-		return Groups.find({_id: {$in: groups}}).fetch();
-	}
-});
