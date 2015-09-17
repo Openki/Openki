c1a1492aa6d6fab56311d1bf02abcf9c3f41316a
1u <urban@xiala.net>
2015-09-17 12:15:44 +0200

    adding Japanese language selector and english string per language

--- client/views/lang.js
+++ client/views/lang.js
@@ -1,12 +1,13 @@
 var lgs = {
-	'ar': { lg: 'ar', name: 'العربية', short: 'العربية'},
-	'de': { lg: 'de', name: 'Deutsch', short: 'de'},
-	'en': { lg: 'en', name: 'English', short: 'en'},
-	'es': { lg: 'es', name: 'Castellano', short: 'es'},
-	'fr': { lg: 'fr', name: 'Français', short: 'fr'},
-	'it': { lg: 'it', name: 'Italiano', short: 'it'},
-	'da': { lg: 'da', name: 'Dansk', short: 'da'},
-	'zh_TW': { lg: 'zh_TW', name: '國語', short: '國語'}
+	'ar': { lg: 'ar', name: 'العربية', short: 'العربية', english:'Arabic'},
+	'da': { lg: 'da', name: 'Dansk', short: 'da', english:'Danish'},
+	'de': { lg: 'de', name: 'Deutsch', short: 'de', english:'German'},
+	'en': { lg: 'en', name: 'English', short: 'en', english:'English'},
+	'es': { lg: 'es', name: 'Castellano', short: 'es', english:'Spanish'},
+	'fr': { lg: 'fr', name: 'Français', short: 'fr', english:'French'},
+	'it': { lg: 'it', name: 'Italiano', short: 'it', english:'Italian'},
+	'ja': { lg: 'ja', name: '日本語', short: '日本語', english:'Japanese'},
+	'zh_TW': { lg: 'zh_TW', name: '國語', short: '國語', english:'Guóyǔ, Taiwanese'}
 };
 
 Template.lang_sel.helpers({
