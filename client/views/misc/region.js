f592b06e36c96ad912bd2d6fb3c605b9c6f42e11
sb <sb@cis.ch>
2015-11-11 14:57:10 +0100

    go to homepage on region change
    fixes #252

--- client/views/misc/region.js
+++ client/views/misc/region.js
@@ -11,10 +11,15 @@ Template.region_sel.helpers({
 
 Template.region_sel.events({
 	'click a.regionselect': function(e){
-		var region_id = this._id ? this._id : 'all'
+		var region_id = this._id ? this._id : 'all';
+		var changed = Session.get('region') !== region_id;
+
 		localStorage.setItem("region", region_id); // to survive page reload
 		Session.set('region', region_id)
-		//return false
+
+		// Go to the homepage when the region is changed
+		if (changed) Router.go('/');
+
 		e.preventDefault();
 	}
 })
