135f0d5d30767ad7e349db3933ecffcbf66619c0
1u <urban@xiala.net>
2015-11-26 02:29:46 +0100

    typo in testdata

--- server/testData/testing.locations.js
+++ server/testData/testing.locations.js
@@ -7,7 +7,7 @@ testlocations = [{
 	'categories': 		['infoshop'],
 	'maxPeople': 		'40',
 	'maxWorkplaces':	'15',
-	'adress': 			'Militärstrasse 87a, 8004 Zürich, Switzerland',
+	'address': 			'Militärstrasse 87a, 8004 Zürich, Switzerland',
 	'route':			'Bus 31 oder 32 bis Militär/Langstrasse, dann zu Fuss Militärstrasse Richtung HB folgen, rechte Strassenseite bis 87a, im Hinterhof',
 	'description': 		'Lokaler Infoladen seit 1990',
 	'contact':			{
@@ -38,7 +38,7 @@ testlocations = [{
 	'categories': 		['hackerspace'],
 	'maxPeople': 		'20',
 	'maxWorkplaces':	'20',
-	'adress': 			'Hohlstrasse 52, 8004 Zürich',
+	'address': 			'Hohlstrasse 52, 8004 Zürich',
 	'route':			'3 min von HelvetiaPlatz, (gleiches Gebäude wie Pusterla-Elektronik)',
 	'description': 		'Das MechArtLab beinhaltet Werkstatt, Schulungsraum, Techn. Bibliothek und Lager und bietet Platz für selbstorganisierte Gruppentreffen, Workshops, Jugendelektronikwerkstatt, uvm.',
 	'contact':			{
@@ -195,7 +195,7 @@ testlocations = [{
 	'categories': 		[''],
 	'maxPeople': 		'',
 	'maxWorkplaces':	'',
-	'adress': 			'',
+	'address': 			'',
 	'route':			'',
 	'description': 		'',
 	'contact':			{
