e7e9754da203a3fcff39dd14f08f7bb43818c451
1u <urban@xiala.net>
2015-11-26 01:41:56 +0100

    style location selection #221 #190

--- client/views/events/event.editLocation.js
+++ client/views/events/event.editLocation.js
@@ -113,8 +113,13 @@ Template.eventEditLocation.helpers({
 	},
 
 	candidateClass: function() {
-		return 'locCandy -locationCandidate' + (this.hover ? ' hover' : '');
+		return 'locationCandidate -locationCandidate' + (this.hover ? ' hover' : '');
+	},
+
+	searching: function() {
+		return !!Template.instance().location.get().name;
 	}
+
 });
 
 
