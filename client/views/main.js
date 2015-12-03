9a9dfb92ae43b6d949f3deef319e6420abff2bb2
1u <urban@xiala.net>
2015-12-03 04:31:21 +0100

    route to startpage if clicking on logo

--- client/views/main.js
+++ client/views/main.js
@@ -14,13 +14,15 @@ Template.layout.helpers({
 });
 
 Template.layout.onCreated(function() {
-	this.showIntro = new ReactiveVar(Session.get("showIntro"));
+	this.showIntro = new ReactiveVar(!Session.get("introShown"));
 });
 
 Template.layout.events({
 	"click .-toggleIntro": function(event, instance) {
+		if (Router.current().route.options.template != "find") return true;
 		event.stopImmediatePropagation();
 		instance.showIntro.set(!instance.showIntro.get());
+		Session.set("introShown", true);
 		return false
 	},
 	"click .-introContainer": function(event, instance) {
