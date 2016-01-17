2d74d185bdf681f3835abdc5c76c8189578663b8
lu40 <lucaobertuefer@mailbox.org>
2016-01-16 21:47:20 -0600

    refine navigation hint style

--- client/views/navigation/navigation.navbar.js
+++ client/views/navigation/navigation.navbar.js
@@ -1,18 +1,18 @@
 Template.navbar.onRendered(function() {
 	this.$('.dropdown').on('show.bs.dropdown', function(e){
-		$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+		$(this).find('.dropdown-menu').first().stop(true, true).show();
 	});
 
 	this.$('.dropdown').on('hide.bs.dropdown', function(e){
-		$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+		$(this).find('.dropdown-menu').first().stop(true, true).hide();
 	});
 
 	$(window).scroll(function (event) {
-		if($(window).scrollTop() > 5){
+		if($(window).scrollTop() > 1){
 			this.$('.navbar-container').addClass('over_content');
 			this.$('a.nav_link.active').addClass('over_content');
 		}
-		else if($(window).scrollTop() < 5){
+		else if($(window).scrollTop() < 1){
 			this.$('.navbar-container').removeClass('over_content');
 			this.$('a.nav_link.active').removeClass('over_content');
 		}
