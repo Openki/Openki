f0b6558eeb060900a3ff55c81b14f62668a180b7
1u <urban@xiala.net>
2016-01-27 00:52:35 +0100

    remove duplicate code for region

--- client/views/find/find.js
+++ client/views/find/find.js
@@ -2,15 +2,6 @@ function finderRoute(path) {
 	return {
 		path: path,
 		template: 'find',
-		onBeforeAction: function() {
-			// Allow setting the region in the URL by parameter '?region=Testistan'
-			if (this.params.query.region) {
-				var region = Regions.findOne({ name: this.params.query.region })
-				if (region) Session.set('region', region._id);
-			};
-
-			this.next();
-		},
 		data: function() {
 			return this.params;
 		},
