df4d3173f36aa983810d6ebecd4b59389ca108a9
1u <urban@xiala.net>
2016-02-17 01:06:13 +0100

    housekeeping applying class-rules

--- client/editable/helpers.js
+++ client/editable/helpers.js
@@ -76,14 +76,14 @@ Template.editable.helpers({
 });
 
 Template.editable.events({
-	'click .editable-store': function(event, instance) {
+	'click .-editableStore': function(event, instance) {
 		instance.changed.set(false);
 		var editable = instance.$('.editable');
 		var changedText = instance.data.simple ? editable.text() : editable.html();
 		instance.data.store(changedText);
 		instance.editingVersion = false;
 	},
-	'click .editable-cancel': function(event, instance) {
+	'click .-editableCancel': function(event, instance) {
 		instance.$('.editable').html(instance.data.text);
 		instance.changed.set(false);
 		instance.editingVersion = false;
