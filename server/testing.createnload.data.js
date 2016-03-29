2a06e3dc51d70d4af0b7e6e877e4ce2d1f46d252
1u <urban@xiala.net>
2016-03-30 01:36:46 +0200

    adding internal-flag to random 7% of test-events

--- server/testing.createnload.data.js
+++ server/testing.createnload.data.js
@@ -228,13 +228,14 @@ createEventsIfNone = function(){
 					ensureRoom(event.location, event.room);
 				}
 
+				event.internal = Math.random() < 0.07;
+
 				event.course_id = course._id;
 				event.title = course.name + ' ' + _.sample(words);
 				event.description =  words.slice(0, 10 + Math.floor(Math.random() * 30)).join(' ');
 				event.groups = course.groups;
 				event.mentors = [];
 				event.host = [];
-				event.internal = false;
 
 				var spread = 1000*60*60*24*365*1.24;              // cause it's millis  1.2 Years
 				var timeToGo = Math.random()-0.7;             // put 70% in the past
