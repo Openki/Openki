5488db4efb1a874be6b39d9a8096735a82b9117c
Stephan Balmer <sb@cis.ch>
2016-04-26 15:22:15 +0200

    rework the group nami logic

--- client/views/groups/groupList.js
+++ client/views/groups/groupList.js
@@ -1,12 +1,21 @@
+var subbedGroup = function(group) {
+	var groupId = ''+group; // it's not a string?! LOL I DUNNO
+	miniSubs.subscribe('group', groupId);
+	return Groups.findOne(groupId);
+};
+
 Template.groupName.helpers({
-	name: function(getFullName) {
+	short: function() {
 		if (!this) return;
-		var groupId = ''+this; // it's not a string?! LOL I DUNNO
-		miniSubs.subscribe('group', groupId);
-		var group = Groups.findOne(groupId);
-		if (!group) return "removed group";
-		if (getFullName) return group && group.name;
-		return group && group.short;
+		var group = subbedGroup(this);
+		if (!group) return "-";
+		return group.short;
+	},
+	name: function() {
+		if (!this) return;
+		var group = subbedGroup(this);
+		if (!group) return mf('group.missing', "Group does not exist");
+		return group.name;
 	},
 });
 
