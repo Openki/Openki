37148dcfe8862f82cb27fed5a57db2bc5153013c
Stephan Balmer <sb@cis.ch>
2016-05-03 21:50:27 +0200

    silence jshint

--- client/views/events/event.edit.js
+++ client/views/events/event.edit.js
@@ -220,7 +220,7 @@ Template.eventEdit.events({
 		instance.files = null;
 
 		//check if file object is stored in the template object
-		if(fileList != null){
+		if (fileList !== null) {
 			var tmp = [];
 			if(this.files){
 				$.each( this.files, function( i,fileObj ){
