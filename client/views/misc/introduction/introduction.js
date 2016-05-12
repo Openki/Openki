e2d612deab13004d029562a6756fbc3f842f6bf3
Stephan Balmer <sb@cis.ch>
2016-05-12 17:24:01 +0200

    clean the introduction logic a bit

--- client/views/misc/introduction/introduction.js
+++ client/views/misc/introduction/introduction.js
@@ -1,15 +1,11 @@
-Template.layout.onCreated(function() {
-	this.showIntro = new ReactiveVar(Assistant.showIntro());
-	this.openedIntro = new ReactiveVar(Assistant.openedIntro());
-});
-
 Template.layout.onRendered(function() {
-	if (!this.openedIntro.get()) {
+	if (!Assistant.openedIntro()) {
 		this.$('.content').hide();
 	}
+
 	var instance = this;
 	this.autorun(function() {
-		if (instance.openedIntro.get()) {
+		if (Assistant.openedIntro()) {
 			instance.$('.content').slideDown(400);
 		} else {
 			instance.$('.content').slideUp(400);
@@ -19,54 +15,35 @@ Template.layout.onRendered(function() {
 
 
 Template.layout.helpers({
-	showIntro: function() {
-		return Template.instance().showIntro.get();
+	shownIntro: function() {
+		return Assistant.shownIntro();
 	}
 });
 
 Template.introduction.helpers({
 	openedIntro: function() {
-		return Template.instance().parentInstance().openedIntro.get();
+		return Assistant.openedIntro();
 	}
 });
 
 
 Template.layout.events({
-	"click .-toggleIntro": function(event, instance) {
-		instance.openedIntro.set(true);
-		if (Router.current().route.options.template === "find") {
-			// Show or hide the intro
-			instance.showIntro.set(!instance.showIntro.get());
-
-			// When we're on the homepage we don't switch
-			event.stopImmediatePropagation();
-			return false;
-		} else {
-			instance.showIntro.set(true);
+	// Clicks on the logo toggle the intro blurb, but only when already on home
+	'click .-toggleIntro': function() {
+		if (Router.current().route.options.template === "findWrap") {
+			Assistant.showIntro();
 		}
 	},
 
-	"click .-closeIntroAtHome": function(event, instance) {
-		var routerTemplate = Router.current().route.options.template;
-		if (routerTemplate === "find") {
-			instance.showIntro.set(false);
-		}
-	},
-
-	"click .-closeIntro": function(event, instance) {
-		instance.showIntro.set(false);
+	"click .js-hide-intro": function(event, instance) {
 		Assistant.doneIntro();
 	},
 
-/*	"click .-introContainer": function(event, instance) {
-		event.stopImmediatePropagation();
-	},
-
-	"click #wrap": function(event, instance) {
-		instance.showIntro.set(false);
-	},
-*/
-	"click .-toggleDetails": function(event, instance) {
-		instance.openedIntro.set(!instance.openedIntro.get());
+	"click .js-toggle-details": function(event, instance) {
+		if (Assistant.openedIntro()) {
+			Assistant.closeIntro();
+		} else {
+			Assistant.openIntro();
+		}
 	}
 });
