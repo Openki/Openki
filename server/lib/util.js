3e8b7ed84d20f50f5f21af9bcda3c5c27dc425f8
Stephan Balmer <sb@cis.ch>
2016-05-24 05:39:52 +0200

    stub untilClean on the client

--- server/lib/util.js
+++ server/lib/util.js
@@ -5,20 +5,3 @@ logAsyncErrors = function(err, ret) {
 	}
 	return ret;
 };
-
-// Repeatedly apply a function until it reports no update
-// The wrapped function will be called repeatedly until it returns true
-// This helps ensuring racing cache updates are settled with
-// the last version winning
-untilClean = function(clean) {
-	var tries = 0;
-	for (; tries < 3; tries++) {
-		if (clean()) return;
-	}
-
-	// Ooops we ran out of tries.
-	// This either means the updates to the cached fields happen faster than
-	// we can cache them (then the cache updates would have to be throttled) or
-	// that the clean function is broken (much more likely).
-	throw new Error("Giving up after trying to apply cleansing function "+tries+" times.");
-};
