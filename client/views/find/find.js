5737432a367d8f3abf0e1a1fa330af80dffec01a
lu40 <lucaobertuefer@mailbox.org>
2016-06-11 09:17:24 -0500

    trigger filterPreviwe animations via css

--- client/views/find/find.js
+++ client/views/find/find.js
@@ -129,8 +129,13 @@ var updateCategorySearch = function(event, instance) {
 	instance.categorySearchResults.set(results);
 };
 
-var filterPreview = function(highlightClass, opacity) {
-	$('.course').not(highlightClass).stop().fadeTo('slow', opacity);
+var filterPreview = function(switchOn, match) {
+	var noMatch = $('.course').not(match);
+	if (switchOn) {
+		noMatch.addClass('filter-no-match');
+	} else {
+		noMatch.removeClass('filter-no-match');
+	}
 };
 
 Template.find.events({
@@ -155,27 +160,43 @@ Template.find.events({
 	},
 
 	'mouseover .js-filter-upcoming-events': function() {
-		filterPreview('.hasupcomingevents', 0.33);
+		filterPreview(true, '.hasupcomingevents');
 	},
 
 	'mouseout .js-filter-upcoming-events': function() {
-		filterPreview('.hasupcomingevents', 1);
+		filterPreview(false, '.hasupcomingevents');
 	},
 
 	'mouseover .js-filter-needs-host': function() {
-		filterPreview('.needsHost', 0.33);
+		filterPreview(true, '.needsHost');
 	},
 
 	'mouseout .js-filter-needs-host': function() {
-		filterPreview('.needsHost', 1);
+		filterPreview(false, '.needsHost');
 	},
 
 	'mouseover .js-filter-needs-mentor': function() {
-		filterPreview('.needsMentor', 0.33);
+		filterPreview(true, '.needsMentor');
 	},
 
 	'mouseout .js-filter-needs-mentor': function() {
-		filterPreview('.needsMentor', 1);
+		filterPreview(false, '.needsMentor');
+	},
+
+	'mouseover .js-category-label': function() {
+		filterPreview(true, ('.'+this));
+	},
+
+	'mouseout .js-category-label': function() {
+		filterPreview(false, ('.'+this));
+	},
+
+	'mouseover .group': function() {
+		filterPreview(true, ('.'+this));
+	},
+
+	'mouseout .group': function() {
+		filterPreview(false, ('.'+this));
 	},
 
 	'keyup .js-search-categories': _.debounce(updateCategorySearch, 100),
@@ -198,14 +219,6 @@ Template.find.events({
 		updateUrl(event, instance);
 	},
 
-	'mouseover .js-category-label': function() {
-		filterPreview(('.'+this), 0.33);
-	},
-
-	'mouseout .js-category-label': function() {
-		filterPreview(('.'+this), 1);
-	},
-
 	'click .js-remove-category-btn': function(event, instance) {
 		instance.filter.remove('categories', ''+this).done();
 		updateUrl(event, instance);
@@ -221,14 +234,6 @@ Template.find.events({
 			instance.filter.done();
 			updateUrl(event, instance);
 		}
-	},
-
-	'mouseover .group': function() {
-		filterPreview(('.'+this), 0.33);
-	},
-
-	'mouseout .group': function() {
-		filterPreview(('.'+this), 1);
 	},
 
 	"click .js-all-regions-btn": function(event, template){
