fb7134dc2d41509465e6dc13e80eca46c6e909de
lu40 <lucaobertuefer@mailbox.org>
2016-06-22 01:21:13 +0200

    Revert "move grouplist from coursedetails to designated template"
    This reverts commit a9f6880defa3f3ba5ed0dc12795a247ce3acb0fb.

--- client/views/courses/details/course.details.js
+++ client/views/courses/details/course.details.js
@@ -149,3 +149,105 @@ Template.courseDetailsPage.events({
 		Router.go('showCourse', this, { query: {edit: 'course'} });
 	}
 });
+
+
+Template.courseGroupList.helpers({
+	'isOrganizer': function() {
+		return Template.instance().data.groupOrganizers.indexOf(_id(this)) >= 0;
+	},
+	'tools': function() {
+		var tools = [];
+		var user = Meteor.user();
+		var groupId = String(this);
+		var course = Template.parentData();
+		if (user && user.mayPromoteWith(groupId) || course.editableBy(user)) {
+			tools.push({
+				toolTemplate: Template.courseGroupRemove,
+				groupId: groupId,
+				course: course,
+			});
+		}
+		if (user && course.editableBy(user)) {
+			var hasOrgRights = course.groupOrganizers.indexOf(groupId) > -1;
+			tools.push({
+				toolTemplate: hasOrgRights ? Template.courseGroupRemoveOrganizer : Template.courseGroupMakeOrganizer,
+				groupId: groupId,
+				course: course,
+			});
+		}
+		return tools;
+	},
+});
+
+
+
+TemplateMixins.Expandible(Template.courseGroupAdd);
+Template.courseGroupAdd.helpers(groupNameHelpers);
+Template.courseGroupAdd.helpers({
+	'groupsToAdd': function() {
+		var user = Meteor.user();
+		return user && _.difference(user.groups, this.groups);
+	}
+});
+
+
+Template.courseGroupAdd.events({
+	'click .js-add-group': function(event, instance) {
+		Meteor.call('course.promote', instance.data._id, event.target.value, true, function(error) {
+			if (error) {
+				addMessage(mf('course.group.addFailed', "Failed to add group"), 'danger');
+			} else {
+				addMessage(mf('course.group.addedGroup', "Added your group to the list of promoters"), 'success');
+				instance.collapse();
+			}
+		});
+	}
+});
+
+
+TemplateMixins.Expandible(Template.courseGroupRemove);
+Template.courseGroupRemove.helpers(groupNameHelpers);
+Template.courseGroupRemove.events({
+	'click .js-remove': function(event, instance) {
+		Meteor.call('course.promote', instance.data.course._id, instance.data.groupId, false, function(error) {
+			if (error) {
+				addMessage(mf('course.group.removeFailed', "Failed to remove group"), 'danger');
+			} else {
+				addMessage(mf('course.group.removedGroup', "Removed group from the list of promoters"), 'success');
+				instance.collapse();
+			}
+		});
+	}
+});
+
+
+TemplateMixins.Expandible(Template.courseGroupMakeOrganizer);
+Template.courseGroupMakeOrganizer.helpers(groupNameHelpers);
+Template.courseGroupMakeOrganizer.events({
+	'click .js-makeOrganizer': function(event, instance) {
+		Meteor.call('course.editing', instance.data.course._id, instance.data.groupId, true, function(error) {
+			if (error) {
+				addMessage(mf('course.group.makeOrganizerFailed', "Failed to give group editing rights"), 'danger');
+			} else {
+				addMessage(mf('course.group.groupMadeOrganizer', "Group members can now edit this"), 'success');
+				instance.collapse();
+			}
+		});
+	}
+});
+
+
+TemplateMixins.Expandible(Template.courseGroupRemoveOrganizer);
+Template.courseGroupRemoveOrganizer.helpers(groupNameHelpers);
+Template.courseGroupRemoveOrganizer.events({
+	'click .js-removeOrganizer': function(event, instance) {
+		Meteor.call('course.editing', instance.data.course._id, instance.data.groupId, false, function(error) {
+			if (error) {
+				addMessage(mf('course.group.removeOrganizerFailed', "Failed to remove organizer status"), 'danger');
+			} else {
+				addMessage(mf('course.group.removedOrganizer', "Removed editing rights"), 'success');
+				instance.collapse();
+			}
+		});
+	}
+});
