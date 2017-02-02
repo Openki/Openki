792a790511f9b11f9266f1edacd9b8f9e1432e9a
lu40 <lucaobertuefer@mailbox.org>
2017-02-02 03:07:21 +0100

    rename frames eventsList to calendar and calendar to week

--- client/views/frames/calendar/frame.calendar.js
+++ client/views/frames/calendar/frame.calendar.js
@@ -11,8 +11,10 @@ Router.map(function () {
 
 Template.frameCalendar.onCreated(function() {
 	var instance = this;
+
 	instance.startOfWeek = new ReactiveVar();
-	instance.weekdays = new ReactiveVar([]);
+	instance.groupedEvents = new ReactiveVar([]);
+	instance.days = new ReactiveVar([]);
 
 	this.autorun(function() {
 		minuteTime.get();
@@ -27,60 +29,40 @@ Template.frameCalendar.onCreated(function() {
 		var filterParams = filter.toParams();
 		var startOfWeek = instance.startOfWeek.get();
 		filterParams.after = startOfWeek.toDate();
-		filterParams.before = moment(startOfWeek).add(1, 'week').toDate();
 
 		instance.subscribe('eventsFind', filterParams, 200);
-	});
 
-	this.autorun(function() {
-		var filter = Filtering(EventPredicates)
-		             .read(Router.current().params.query)
-		             .done();
+		var events = Events.find({}, {sort: {start: 1}}).fetch();
+		var groupedEvents = _.groupBy(events, function(event) {
+			return moment(event.start).format('LL');
+		});
 
-		var start = instance.startOfWeek.get();
-		var end = moment(start).add(1, 'week');
-
-		var weekdays = [];
-		var current = moment(start);
-		while (current.isBefore(end)) {
-			var next = moment(current).add(1, 'day');
-			var filterParams = filter.toParams();
-			filterParams.after = current.toDate();
-			filterParams.before = next.toDate();
-
-			weekdays.push({
-				date: current,
-				dayEvents: eventsFind(filterParams, 200)
-			});
-			current = next;
-		}
-		instance.weekdays.set(weekdays);
+		instance.groupedEvents.set(groupedEvents);
+		instance.days.set(Object.keys(groupedEvents));
 	});
 });
 
 Template.frameCalendar.helpers({
-	calendarDay: function(day) {
-		Session.get('timeLocale');
-		return moment(day.toDate()).format('dddd, Do MMMM');
+	'ready': function() {
+		return Template.instance().subscriptionsReady();
 	},
 
-	hasDayEvents: function() {
-		return this.dayEvents.count() > 0;
+	'days': function() {
+		return Template.instance().days.get();
 	},
 
-	weekdays: function() {
-		return Template.instance().weekdays.get();
+	'eventsOn': function(day) {
+		var groupedEvents = Template.instance().groupedEvents.get();
+		return groupedEvents[day];
 	}
 });
 
-Template.frameCalendar.onRendered(function() {
-	var instance = this;
-	this.autorun(function() {
-		// rerun when subscriptions become ready
-		instance.subscriptionsReady();
-		// wait until subtemplates are rendered
-		setTimeout(function(){
-			instance.$("a").attr("target", "_blank");
-		}, 0);
-	});
+Template.frameCalendarEvent.events({
+	'click .js-toggle-event-details': function(e, instance) {
+		var jQueryTarget = $(e.currentTarget);
+
+		jQueryTarget.toggleClass('active');
+		jQueryTarget.nextAll('.frame-calendar-event-body').toggle();
+		jQueryTarget.children('.frame-calendar-event-time').toggle();
+	}
 });
