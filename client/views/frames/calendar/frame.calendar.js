4def81408a89cecebf803e6872fa4250822683fc
lu40 <lucaobertuefer@mailbox.org>
2017-02-16 18:40:45 +0100

    make CssRules more general appliable and use it as an import

--- client/views/frames/calendar/frame.calendar.js
+++ client/views/frames/calendar/frame.calendar.js
@@ -1,53 +1,15 @@
+import '/client/views/frames/imports/CssRules.js';
+
 Router.map(function () {
 	this.route('frameCalendar', {
 		path: '/frame/calendar',
 		template: 'frameCalendar',
 		layoutTemplate: 'frameCalendar',
 		data: function() {
-			var customizableProperties = [];
-			customizableProperties.add = function(key, name, selector) {
-				this.push({
-					key: key,
-					name: name,
-					selector: selector
-				});
-				return this;
-			};
-
-			customizableProperties
-				.add('bgcolor', 'background-color', 'body')
-				.add('color', 'color', 'body')
-				.add('eventbg', 'background-color', '.frame-calendar-event')
-				.add('eventcolor', 'color', '.frame-calendar-event')
-				.add('linkcolor', 'color', '.frame-calendar-event a')
-				.add('fontsize', 'font-size', '*');
-
-			var cssRules = [];
-			var query = this.params.query;
-			_.forEach(customizableProperties, function(property) {
-				var queryValue = query[property.key];
-				var cssValue;
-				if (typeof queryValue !== 'undefined') {
-					// hexify color values
-					if (property.name.indexOf('color') >= 0) {
-						if (queryValue.match(/^[0-9A-F]+$/i)) {
-							cssValue = '#' + queryValue.substr(0, 6);
-						}
-					} else {
-						var intVal = parseInt(queryValue, 10);
-						if (!Number.isNaN(intVal)) {
-							cssValue = Math.max(0, Math.min(1000, intVal)) + 'px';
-						}
-					}
-
-					if (cssValue) {
-						cssRules.push({ selector: property.selector, name: property.name, value: cssValue });
-					}
-				}
-			});
+			var cssRules = new CssRules();
+			cssRules.read(this.params.query);
 
 			return { cssRules: cssRules };
-
 		},
 		onAfterAction: function() {
 			document.title = webpagename + ' Calendar';
