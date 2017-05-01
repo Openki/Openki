1edc8f380b0d3d5778d5fb25ea059e5211d5d633
Stephan Balmer <sb@cis.ch>
2017-05-01 16:07:14 +0200

    loop directly over cursor and return updated count

--- server/updates/2017.04.27 ensurePostTimeUpdatedField.js
+++ server/updates/2017.04.27 ensurePostTimeUpdatedField.js
@@ -1,13 +1,16 @@
 UpdatesAvailable.ensurePostTimeUpdatedField = function() {
+	var count = 0;
+
 	// Until now it is not possible to reference a document field during an
 	// update. Therefore we have to fetch the results and iterate over them.
 	// https://stackoverflow.com/questions/3788256#3792958
-	var fetchedPosts = CourseDiscussions.find({ time_updated: null }).fetch();
-
-	_.each(fetchedPosts, function(post) {
+	CourseDiscussions.find({ time_updated: null }).forEach(function(post) {
+		count += 1;
 		CourseDiscussions.update(
 			{ _id: post._id },
 			{ time_updated: post.time_created }
 		);
 	});
+
+	return count;
 };
