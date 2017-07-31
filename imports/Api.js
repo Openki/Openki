a00d02cbfbcb8ae3f119d895098da61f1136fa58
Stephan Balmer <sb@cis.ch>
2017-07-31 19:49:02 +0200

    validate API filter params and switch to jSend conventions

--- imports/Api.js
+++ imports/Api.js
@@ -1,18 +1,47 @@
+var jSendResponder = function(res, process) {
+	try {
+		var data = process();
+		let body =
+			{ status: "success"
+			, data: process()
+			};
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+		res.end(JSON.stringify(body, null, "\t"));
+	} catch(e) {
+		let body = {};
+		if (e instanceof FilteringReadError) {
+			res.statusCode = 400;
+			body.status = "fail";
+			body.data = {};
+			if (e.name) {
+				body.data[e.name] = e.message;
+			} else {
+				body.data.error = e.message;
+			}
+		} else {
+			res.statusCode = 500;
+			body.status = "error";
+			body.message = "Server error";
+		}
+		res.end(JSON.stringify(body, null, "\t"));
+	}
+}
 
 Router.map(function () {
 	this.route('api.0.json.groups', {
 		path: '/api/0/json/groups',
 		where: 'server',
 		action: function () {
-			var groupQuery = Filtering(GroupPredicates).read(this.params.query).done().toQuery();
-			var groups = GroupLib.find(groupQuery).fetch();
+			jSendResponder(this.response, () => {
+				var groupQuery = Filtering(GroupPredicates).readAndValidate(this.params.query).done().toQuery();
+				var groups = GroupLib.find(groupQuery).fetch();
 
-			_.each(groups, function(group) {
-				group.link = Router.url('groupDetails', group);
+				_.each(groups, function(group) {
+					group.link = Router.url('groupDetails', group);
+				});
+				return groups;
 			});
-
-			this.response.setHeader('Content-Type', 'application/json; charset=utf-8');
-			this.response.end(JSON.stringify(groups, null, "\t"));
 		}
 	});
 });
@@ -22,15 +51,14 @@ Router.map(function () {
         path: '/api/0/json/venues',
         where: 'server',
         action: function () {
-            var venueQuery = Filtering(VenuePredicates).read(this.params.query).done().toQuery();
-            var venues = Venues.find(venueQuery).fetch();
-
-			_.each(venues, function(venue) {
-				venue.link = Router.url('venueDetails', venue);
+			jSendResponder(this.response, () => {
+	            var venueQuery = Filtering(VenuePredicates).readAndValidate(this.params.query).done().toQuery();
+	            var venues = Venues.find(venueQuery).fetch();
+				_.each(venues, function(venue) {
+					venue.link = Router.url('venueDetails', venue);
+				});
+				return venues;
 			});
-
-            this.response.setHeader('Content-Type', 'application/json; charset=utf-8');
-            this.response.end(JSON.stringify(venues, null, "\t"));
         }
     });
 });
@@ -40,64 +68,64 @@ Router.map(function () {
         path: '/api/0/json/events',
         where: 'server',
         action: function () {
-            var eventQuery = Filtering(EventPredicates).read(this.params.query).done().toQuery();
-            var events = eventsFind(eventQuery).fetch();
+			jSendResponder(this.response, () => {
+	            var eventQuery = Filtering(EventPredicates).readAndValidate(this.params.query).done().toQuery();
+	            var events = eventsFind(eventQuery).fetch();
 
-			var result = _.map(events, function(ev) {
-				var evr =
-					{ id: ev._id
-					, title: ev.title
-					, description: ev.description
-					, startLocal: ev.startLocal
-					, endLocal: ev.endLocal
-					, start: ev.start
-					, end: ev.end
-					, duration: moment(ev.end).diff(ev.start) / 60000 // Minutes
-					, link: Router.url('showEvent', ev)
-					, internal: ev.internal
-					, room: ev.room
-					};
-
-				if (ev.venue) {
-					evr.venue =
-						{ id: ev.venue._id
-						, name: ev.venue.name
-						, loc: ev.venue.loc
-						, link: Router.url('venueDetails', ev.venue)
+				var result = _.map(events, function(ev) {
+					var evr =
+						{ id: ev._id
+						, title: ev.title
+						, description: ev.description
+						, startLocal: ev.startLocal
+						, endLocal: ev.endLocal
+						, start: ev.start
+						, end: ev.end
+						, duration: moment(ev.end).diff(ev.start) / 60000 // Minutes
+						, link: Router.url('showEvent', ev)
+						, internal: ev.internal
+						, room: ev.room
 						};
-				}
 
-				if (ev.courseId) {
-					let course = Courses.findOne(ev.courseId);
-					if (course) {
-						evr.course =
-							{ id: ev.courseId
-							, name: course.name
-							, link: Router.url('showCourse', course)
+					if (ev.venue) {
+						evr.venue =
+							{ id: ev.venue._id
+							, name: ev.venue.name
+							, loc: ev.venue.loc
+							, link: Router.url('venueDetails', ev.venue)
 							};
 					}
-				}
 
-				evr.groups = [];
-				var groups = ev.groups || [];
-				for(var groupId of groups) {
-					let group = Groups.findOne(groupId);
-					if (group) {
-						evr.groups.push(
-							{ id: group._id
-							, name: group.name
-							, short: group.short
-							, link: Router.url('groupDetails', group)
-							}
-						);
+					if (ev.courseId) {
+						let course = Courses.findOne(ev.courseId);
+						if (course) {
+							evr.course =
+								{ id: ev.courseId
+								, name: course.name
+								, link: Router.url('showCourse', course)
+								};
+						}
 					}
-				};
 
-				return evr;
+					evr.groups = [];
+					var groups = ev.groups || [];
+					for(var groupId of groups) {
+						let group = Groups.findOne(groupId);
+						if (group) {
+							evr.groups.push(
+								{ id: group._id
+								, name: group.name
+								, short: group.short
+								, link: Router.url('groupDetails', group)
+								}
+							);
+						}
+					};
+
+					return evr;
+				});
+				return result;
 			});
-
-            this.response.setHeader('Content-Type', 'application/json; charset=utf-8');
-            this.response.end(JSON.stringify(result, null, "\t"));
         }
     });
 });
