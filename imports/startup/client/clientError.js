786049d2e9dee56cc5ebdf0ae67c4e9e14545787
Stephan Balmer <sb@cis.ch>
2017-09-19 03:47:38 +0200

    also graze the Meteor debug output for template errors

--- imports/startup/client/clientError.js
+++ imports/startup/client/clientError.js
@@ -1,9 +1,50 @@
-window.addEventListener("error", function (event) {
+const reportToServer = function(error) {
 	var report =
-		{ name: event.error.name
-		, message: event.error.message
+		{ name: error.name
+		, message: error.message
 		, location: window.location.href
 		, tsClient: new Date()
 		};
 	Meteor.call('clientError', report);
+}
+
+window.addEventListener("error", function(event) {
+	reportToServer(event.error);
 });
+
+var buffer = [];
+var discriminatoryReporting = function(arguments) {
+	var msg = arguments[0];
+
+	// "Exception from Tracker recompute function:"
+	if (msg.indexOf("Exception ") === 0) {
+		// Boring
+		return;
+	}
+
+	// "Error: No such function: ..."
+	if (msg.indexOf("Error:") === 0) {
+		buffer.push(msg);
+		return;
+	}
+
+	// "Blaze.View.prototy..."
+	if (msg.indexOf("Blaze.") === 0) {
+		// There's a template name in there right?
+		var templateNames = /Template\.[^_]\w+/g;
+		buffer.push(msg.match(templateNames).join(','));
+		reportToServer(
+			{ name: 'TemplateError'
+			, message: buffer.join('; ')
+			}
+		);
+		return;
+	}
+};
+
+// wrap the Meteor debug function
+const meteorDebug = Meteor._debug;
+Meteor._debug = function(/* arguments */) {
+	meteorDebug.apply(this, arguments);
+	discriminatoryReporting(arguments);
+}
