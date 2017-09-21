9d826512e4cb9c5903946f187c8ad324198f8432
Lorenz Schori <lo@znerol.ch>
2017-09-21 21:01:03 +0200

    Only include necessary shariff scss

--- imports/ui/lib/shariff/shariff.js
+++ imports/ui/lib/shariff/shariff.js
@@ -1,5 +1,12 @@
-import Shariff from 'shariff';
 import './scss/shariff-layout.scss';
-import './scss/shariff-services.scss';
 
+import "./scss/services/diaspora";
+import "./scss/services/facebook";
+import "./scss/services/googleplus";
+import "./scss/services/info";
+import "./scss/services/mail";
+import "./scss/services/twitter";
+import "./scss/services/whatsapp";
+
+import Shariff from 'shariff';
 export default Shariff;
