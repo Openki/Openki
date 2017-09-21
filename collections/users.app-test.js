8caa09bb753c3e3e16ced6536d6fda06813486b1
Stephan Balmer <sb@cis.ch>
2017-09-22 00:54:00 +0200

    migrate cenue creation test

--- collections/users.app-test.js
+++ collections/users.app-test.js
@@ -1,23 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { assert } from 'meteor/practicalmeteor:chai';
 
-// Same as greg's
-const seesDigestedPassword = {
-	"digest":"0d2c690e7dd5f94780383e9dfa1f4def044319104ad16ab15e45eeb2a8dfc81b",
-	"algorithm":"sha-256"
-};
-
 if (Meteor.isClient) {
 	describe('Profile', function () {
 		it('accepts login', function (done) {
-			Meteor.call('login',
-				{ "user": { "username": "Seee" }
-				, "password": seesDigestedPassword
-				}
-				, (err, response) => {
-					return done(err);
-				}
-			);
+			Meteor.loginWithPassword("Seee", "greg", (err, response) => {
+				return done(err);
+			});
 		});
 		it('does not allow setting duplicate email', function (done) {
 			Meteor.call('update_userdata',
