4cc479f0c861f542c8f13dbdf36f09e138d65ce3
lu40 <lucaobertuefer@mailbox.org>
2017-10-02 18:29:31 +0200

    add new faq text (still a lot of open todos)

--- client/views/static/FAQ/FAQ.js
+++ client/views/static/FAQ/FAQ.js
@@ -2,5 +2,37 @@ Router.map(function () {
 	this.route('FAQ', {
 		path: '/FAQ',
 		template: 'FAQ',
+		data() {
+			return { hash: this.params.hash };
+		}
 	});
+});
+
+Template.FAQ.onRendered(function() {
+	this.$('.faq-question-title').each(function() {
+		const title = $(this);
+		const id =
+			title
+				.text()
+				.trim()
+				.toLowerCase()
+				.replace(/[_+.,!?@#$%^&*();\\\/|<>"'=]/g, "")
+				.replace(/[ ]/g, "-");
+
+		title.attr('id', id);
+	});
+
+	if (this.data.hash) {
+		const targetTitle = this.$('.faq-question-title#' + this.data.hash);
+		targetTitle.next('.faq-question-answer').show();
+		$(window).scrollTop(targetTitle.position().top - SCSSVars.navbarHeight);
+	}
+});
+
+Template.FAQ.events({
+	'click .js-toggle-answer'(event, instance) {
+		const title = $(event.currentTarget);
+		title.next('.faq-question-answer').toggle();
+		title.toggleClass('active');
+	}
 });
