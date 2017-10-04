a311380e5ee70c64cd8d7c8782131e221610c805
lu40 <lucaobertuefer@mailbox.org>
2017-10-04 22:16:25 +0200

    remove unnecessary code for proposal frame this fixes the bug, that the proposal frame reloads and the data get's deleted

--- client/views/frames/propose/frame.propose.js
+++ client/views/frames/propose/frame.propose.js
@@ -5,26 +5,12 @@ Router.map(function () {
 		path: '/frame/propose',
 		template: 'framePropose',
 		layoutTemplate: 'frameLayout',
-		waitOn: function () {
-			this.filter = Filtering(EventPredicates).read(this.params.query).done();
-
-			var filterParams = this.filter.toParams();
-			filterParams.after = minuteTime.get();
-
-			var limit = parseInt(this.params.query.count, 10) || 6;
-
-			return [
-				Meteor.subscribe('eventsFind', filterParams, limit*2),
-				Meteor.subscribe('regions')
-			];
-		},
-
-		data: function() {
-			return Regions.find();
-		},
-
-		onAfterAction: function() {
-			Metatags.setCommonTags(mf('course.propose.windowtitle', 'Propose new course'));
+		waitOn: () => Meteor.subscribe('regions'),
+		data: () => Regions.find(),
+		onAfterAction() {
+			Metatags.setCommonTags(
+				mf('course.propose.windowtitle', 'Propose new course')
+			);
 		}
 	});
 });
