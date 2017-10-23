dba7a682d209e64f557ee9d5937c1559cba5bd97
Stephan Balmer <sb@cis.ch>
2017-10-23 16:35:21 +0200

    check more failure conditions in visitor-proposals

--- collections/courses.app-test.js
+++ collections/courses.app-test.js
@@ -11,6 +11,8 @@ function promiseMeteorCall(...args) {
 }
 
 if (Meteor.isClient) {
+	const regionId = "9JyFCoKWkxnf8LWPh"; // Testistan
+
 	describe('Course save', function() {
 		it('stores a course', function() {
 			return new Promise((resolve, reject) => {
@@ -19,7 +21,6 @@ if (Meteor.isClient) {
 					else resolve();
 				});
 			}).then(() => {
-				const regionId = "9JyFCoKWkxnf8LWPh"; // Testistan
 				const course = {
 					name: "Intentionally clever title for a generated test-course",
 					description: "This space intentionally filled with bland verbiage. You are safe to ignore this. ",
@@ -52,18 +53,52 @@ if (Meteor.isClient) {
 					else resolve();
 				});
 			}).then(() => {
-				const regionId = "9JyFCoKWkxnf8LWPh"; // Testistan
 				const course = {
 					name: "Visitor's course",
 					description: "Created without login",
 					region: regionId,
 					emailSignup: "visitor+1970@clogged.tubes.example"
 				};
-				return promiseMeteorCall('save_course', '', course).then(
-					(courseId) => ({ course, courseId })
-				);
-			}).then(({ course, courseId }) => {
+				return promiseMeteorCall('save_course', '', course);
+			}).then(courseId => {
 				assert.isString(courseId, "saving course returns a courseId");
+
+				const course = {
+					name: "Visitor's second course",
+					description: "Created without login",
+					region: regionId,
+					emailSignup: "visitor+1970@clogged.tubes.example"
+				};
+				return promiseMeteorCall('save_course', '', course);
+			}).then(courseId => {
+				assert.isString(courseId, "saving second course returns a courseId");
+			});
+		});
+	});
+
+	describe('Prevent attaching visitor-proposals', function() {
+		it('to existing non-visitors', function() {
+			return new Promise((resolve, reject) => {
+				// Make sure client is logged-out
+				// Other tests log-in
+				Meteor.logout((err) => {
+					if (err) reject(err);
+					else resolve();
+				});
+			}).then(() => {
+				const course = {
+					name: "Visitor's course",
+					description: "Created without login",
+					region: regionId,
+					emailSignup: "greg@openki.example" // greg is a non-visitor
+				};
+				return new Promise((resolve, reject) => {
+					Meteor.call('save_course', '', course, err => {
+						resolve(err);
+					});
+				}).then((err) => {
+					assert(!!err, "not allowed to save course");
+				});
 			});
 		});
 	});
