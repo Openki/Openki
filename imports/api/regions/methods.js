e18d0a2e7abcc1bcdc9bf11cfaef9b695a36dbd2
lu40 <lucaobertuefer@mailbox.org>
2017-11-22 01:56:41 +0100

    use es6

--- imports/api/regions/methods.js
+++ imports/api/regions/methods.js
@@ -2,22 +2,20 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.methods({
 	'regions.updateCounters'(selector) {
-		Regions.find(selector).forEach(function(region) {
+		Regions.find(selector).forEach((region) => {
 			// We don't use AsyncTools.untilClean() here because consistency doesn't matter
 			const regionId = region._id;
-			var courseCount = Courses.find({ region: regionId }).count();
-			var futureEventCount = Events.find({ region: regionId, start: { $gte: new Date() } }).count();
-			Regions.update(regionId, { $set: {
-				courseCount: courseCount,
-				futureEventCount: futureEventCount
-			} });
+			const courseCount = Courses.find({ region: regionId }).count();
+			const futureEventCount =
+				Events
+				.find({ region: regionId, start: { $gte: new Date() } })
+				.count();
+
+			Regions.update(regionId, { $set: { courseCount, futureEventCount } });
 		});
 	},
 
 	'region.setFeaturing'(regionId, featuring) {
-		Regions.update(
-			{ _id: regionId },
-			{ $set: { featuring } }
-		);
+		Regions.update(regionId, { $set: { featuring } });
 	}
 });
