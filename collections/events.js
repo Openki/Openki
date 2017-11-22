e526c3a86ad471ef09f7028e2399c09cc3f3e8ad
lu40 <lucaobertuefer@mailbox.org>
2017-11-22 01:49:08 +0100

    move updateRegionCounters method to methods.js

--- collections/events.js
+++ collections/events.js
@@ -359,7 +359,7 @@ Meteor.methods({
 
 			Meteor.call('updateEventVenue', eventId, AsyncTools.logErrors);
 			Meteor.call('event.updateGroups', eventId, AsyncTools.logErrors);
-			Meteor.call('updateRegionCounters', event.region, AsyncTools.logErrors);
+			Meteor.call('regions.updateCounters', event.region, AsyncTools.logErrors);
 
 			// the assumption is that all replicas have the same course if any
 			if (event.courseId) Meteor.call('updateNextEvent', event.courseId, AsyncTools.logErrors);
@@ -381,7 +381,7 @@ Meteor.methods({
 		Events.remove(eventId);
 
 		if (event.courseId) Meteor.call('updateNextEvent', event.courseId);
-		Meteor.call('updateRegionCounters', event.region, AsyncTools.logErrors);
+		Meteor.call('regions.updateCounters', event.region, AsyncTools.logErrors);
 	},
 
 
--- imports/api/regions/methods.js
+++ imports/api/regions/methods.js
@@ -1,6 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.methods({
+	'regions.updateCounters'(selector) {
+		Regions.find(selector).forEach(function(region) {
+			// We don't use AsyncTools.untilClean() here because consistency doesn't matter
+			const regionId = region._id;
+			var courseCount = Courses.find({ region: regionId }).count();
+			var futureEventCount = Events.find({ region: regionId, start: { $gte: new Date() } }).count();
+			Regions.update(regionId, { $set: {
+				courseCount: courseCount,
+				futureEventCount: futureEventCount
+			} });
+		});
+	},
+
 	'region.setFeaturing'(regionId, featuring) {
 		Regions.update(
 			{ _id: regionId },
