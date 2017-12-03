99ec203290444428bf024e7ee8b5aa5b8da20bb5
lu40 <lucaobertuefer@mailbox.org>
2017-12-03 20:34:25 +0100

    don't track featuredGroup, return it from helper

--- imports/ui/components/featured-group/featured-group.js
+++ imports/ui/components/featured-group/featured-group.js
@@ -6,21 +6,21 @@ import Regions from '/imports/api/regions/regions.js';
 import './featured-group.html';
 
 Template.featuredGroup.onCreated(function featuredGroupOnCreated() {
-	this.featuredGroup = new ReactiveVar(false);
 	this.autorun(() => {
-		const currentRegion = Regions.findOne(Session.get('region'));
-		if (currentRegion) {
-			const featuredGroup = currentRegion.featuredGroup;
-			this.subscribe('group', featuredGroup, () => {
-				this.featuredGroup.set(Groups.findOne(featuredGroup));
-			});
-		}
+		const region = Regions.findOne(Session.get('region'));
+		if (region) this.subscribe('group', region.featuredGroup);
 	});
+
+	this.featuredGroupId = () => {
+		const region = Regions.findOne(Session.get('region'));
+		if (region) return region.featuredGroup;
+		return false;
+	};
 });
 
 Template.featuredGroup.helpers({
 	showFeaturedGroup() {
-		if (!Template.instance().featuredGroup.get()) return false;
+		if (!Template.instance().featuredGroupId()) return false;
 
 		// only show featured group on certain pages
 		const routes = ['home', 'find'];
@@ -28,6 +28,10 @@ Template.featuredGroup.helpers({
 		return routes.some(route => router.route && router.route.getName() === route);
 	},
 
-	featuredGroup: () => Template.instance().featuredGroup.get(),
+	featuredGroup: () => {
+		const featuredGroupId = Template.instance().featuredGroupId();
+		if (featuredGroupId) return Groups.findOne(featuredGroupId);
+	},
+
 	regionName: () => Regions.findOne(Session.get('region')).name
 });
