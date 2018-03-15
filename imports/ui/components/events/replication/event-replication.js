9fd34b4db5c910c1a750708126b145efc5d77a83
lu40 <lucaobertuefer@mailbox.org>
2018-03-15 20:45:12 +0100

    remove unused import

--- imports/ui/components/events/replication/event-replication.js
+++ imports/ui/components/events/replication/event-replication.js
@@ -2,7 +2,6 @@ import Events from '/imports/api/events/events.js';
 
 import LocalTime from '/imports/utils/local-time.js';
 import { AddMessage } from '/imports/api/messages/methods.js';
-import ShowServerError from '/imports/ui/lib/show-server-error.js';
 import AffectedReplicaSelectors from '/imports/utils/affected-replica-selectors.js';
 
 import '/imports/ui/components/buttons/buttons.js';
