973327ad28d8b0c29fa24d817e6e3b2191045f1d
lu40 <lucaobertuefer@mailbox.org>
2018-06-04 18:23:29 +0200

    move alert messages to top - now nice fold up/in animations - stay fixed below the navbar - success messages still disappear after 5s

--- imports/ui/components/messages/messages.js
+++ imports/ui/components/messages/messages.js
@@ -5,21 +5,42 @@ import { RemoveMessage } from '/imports/api/messages/methods.js';
 
 import './messages.html';
 
-Template.messages.helpers({
-	hasMessages: () => ClientMessages.find().count(),
-	messages: () => ClientMessages.find()
+Template.messages.onCreated(function() {
+	this.updateSpacerHeight = () => {
+		this.$('.messages-spacer').height(this.$('.messages').height());
+	};
 });
 
-Template.message.onRendered(function messageOnRendered() {
-	const message = this.data;
+Template.messages.helpers({
+	messages() {
+		return ClientMessages.find();
+	}
+});
 
-	if (message.type == 'success') {
-		setTimeout(() => RemoveMessage(message._id), 5000);
+Template.message.onCreated(function() {
+	this.remove = (messageId) => {
+		const $message = this.$('.message');
+		// get 'transition-duration' and convert to miliseconds for fadeOut
+		const duration = parseFloat($message.css('transition-duration')) * 1000;
+		$message.fadeOut(duration, () => {
+			this.parentInstance().updateSpacerHeight();
+			RemoveMessage(messageId);
+		});
+	};
+});
+
+Template.message.onRendered(function() {
+	this.parentInstance().updateSpacerHeight();
+	this.$('.message').toggleClass('is-faded-in');
+
+	const message = Template.currentData();
+	if (message.type === 'success') {
+		this.timedRemove = setTimeout(() => this.remove(message._id), 5000);
 	}
 });
 
 Template.message.events({
-	'click button.close'() {
-		RemoveMessage(this._id);
+	'click .js-remove-message'(event, instance) {
+		instance.remove(this._id);
 	}
 });
